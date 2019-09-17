b38d13143160572af64eda00ad760d78c2a15d00
Manuel De la Penna <manueldelapenna@gmail.com>
2019-09-17 14:53:01 -0300

    avanzo con dm tipos

--- client/dm-tipos.ts
+++ client/dm-tipos.ts
@@ -9,14 +9,23 @@
 
 export type Atributo = {
     atributo:string
+    tipodato:string
     nombreatributo:string
-    tipo:string
+    prodatr__rangodesde: number
+    prodatr__rangohasta: number
+    prodatr__valornormal: number
+    prodatr__normalizable: string
+    normalizable: string
+    escantidad: string
+    prodatr__orden: number
+    opciones: string
+    especificaciones__mostrar_cant_um: string
 }
 
 export type Producto={
     producto:string
-    nombreproducto:string
-    especificacion:string
+    nombreproducto:string  //AGREGAR A CONSULTA (actualmente lo muestra en un objeto llamado productocompleto)
+    especificacioncompleta: string //AGREGAR A CONSULTA (actualmente lo muestra en un objeto llamado productocompleto)
     atributos:{
         [atributo:number]: true
     }
@@ -25,7 +34,8 @@ export type Producto={
 
 export type Formulario={
     formulario:number
-    nombreformulario:string
+    nombreformulario:string //AGREGAR a MOBILE VISITA, actualmente está en HOJA DE RUTA
+    orden: number
     productos:{
         [producto:string]: {
             observaciones: number
@@ -37,31 +47,48 @@ export type Formulario={
 export type Estructura={
     atributos:{[atributo:number]:Atributo}
     productos:{[producto:string]:Producto}
-    formularios:{[fomrulario:number]:Formulario}
+    formularios:{[formulario:number]:Formulario}
 }
 
 export type RelAtr={
-    valorAnterior:string
+    valoranterior:string
     valor:string
+    valornormal: number //AGREGAR A CONSULTA (actualmente viene en PRECIOS MOBILE)
+    prioridad: number //AGREGAR A CONSULTA (actualmente viene en PRECIOS MOBILE)
+    normalizable: string //AGREGAR A CONSULTA (actualmente viene en PRECIOS MOBILE)
+    tiponormalizacion: string //AGREGAR A CONSULTA (actualmente viene en PRECIOS MOBILE)
+    valor_pesos: number //AGREGAR A CONSULTA (actualmente viene en PRECIOS MOBILE)
 }
 
 export type RelPre={
     observaciones:{
         [observacion:number]:{
             precio:number,
-            precioAnterior:number,
-            tipoPrecio:string,
-            tipoPrecioAnterior:string,
+            precioanterior:number,
+            tipopecio:string,
+            tipoprecioanterior:string,
             atributos:{
                 [atributo:number]:RelAtr
             }
+            cambio: string
+            comentariosrelpre: string
+            precionormalizado: number
+            precionormalizado_1: number
+            promobs_1: number
+            normsindato:string
+            fueraderango:string
+            sinpreciohace4meses:string
+            adv: boolean
         }
     }
 }
 
 export type RelVis={
-    razon: number,
-    fechaVisita: number,
+    formulario: number
+    razon: number
+    raz__escierredefinitivoinf: string
+    raz__escierredefinitivofor: string
+    comentarios: string
     productos:{
         [producto:number]:RelPre
     }
