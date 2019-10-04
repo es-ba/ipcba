2948c3a6e3b9904934f540dd2720626e39fd626b
Elba Tonini <ebtonini@gmail.com>
2019-10-04 18:14:07 -0300

    Requerimientos IPCBA 497 y 500

--- server/table-control_comentariosrelpre.js
+++ server/table-control_comentariosrelpre.js
@@ -1,25 +1,42 @@
 "use strict";
 
 module.exports = function(context){
+    var puedeEditar = context.user.usu_rol ==='ingresador' || context.user.usu_rol ==='programador' || context.user.usu_rol ==='recepcionista' || context.user.usu_rol ==='analista' || context.user.usu_rol ==='coordinador' || context.user.usu_rol ==='jefe_campo' || context.user.usu_rol ==='recep_gabinete'|| context.user.usu_rol ==='migracion'|| context.user.usu_rol ==='supervisor';
     return context.be.tableDefAdapt({
         name:'control_comentariosrelpre',
-        editable:false,
-        dbOrigin:'view',
+        tableName:'relpre',
+        editable:puedeEditar,
+        allow:{
+            insert:false,
+            delete:false,
+            update:puedeEditar,
+        },        
         fields:[
-            {name:'periodo'                      ,typeName:'text'   }, 
-            {name:'informante'                   ,typeName:'integer'},
-            {name:'visita'                       ,typeName:'integer'},
-            {name:'panel'                        ,typeName:'integer'},
-            {name:'tarea'                        ,typeName:'integer'},
-            {name:'recepcionista'                ,typeName:'text'   },
-            {name:'nombrerecepcionista'          ,typeName:'text'   },
-            {name:'producto'                     ,typeName:'text'   },
-            {name:'nombreproducto'               ,typeName:'text'   },
-            {name:'observacion'                  ,typeName:'integer'},
-            {name:'tipoprecio'                   ,typeName:'text'   },
-            {name:'esvisiblecomentarioendm'      ,typeName:'boolean', title:'Ver'},
-            {name:'comentariosrelpre'            ,typeName:'text'   },
+            {name:'periodo'                      ,typeName:'text'   , allow:{update:false}}, 
+            {name:'informante'                   ,typeName:'integer', allow:{update:false}},
+            {name:'visita'                       ,typeName:'integer', allow:{update:false}},
+            {name:'panel'                        ,typeName:'integer', allow:{update:false}},
+            {name:'tarea'                        ,typeName:'integer', allow:{update:false}},
+            {name:'recepcionista'                ,typeName:'text'   , allow:{update:false}, title:'rec'},
+            {name:'nombrerecepcionista'          ,typeName:'text'   , allow:{update:false}},
+            {name:'producto'                     ,typeName:'text'   , allow:{update:false}},
+            {name:'nombreproducto'               ,typeName:'text'   , allow:{update:false}},
+            {name:'observacion'                  ,typeName:'integer', allow:{update:false}, title:'obs'},
+            {name:'tipoprecio'                   ,typeName:'text'   , allow:{update:false}, title:'TP'},
+            {name:'esvisiblecomentarioendm'      ,typeName:'boolean', title:'Ver', allow:{update:puedeEditar}},
+            {name:'comentariosrelpre'            ,typeName:'text'   , allow:{update:puedeEditar}, width:500},
         ],
         primaryKey:['periodo','informante','producto','visita','observacion'],
-    });
+        sortColumns:[{column:'periodo'},{column:'panel'},{column:'tarea'},{column:'informante'},{column:'visita'}],        
+        sql:{
+            from:`(SELECT r.periodo, r.informante, r.visita, v.panel, v.tarea, v.recepcionista,
+                   (pr.nombre::text || ' '::text) || pr.apellido::text AS nombrerecepcionista,
+                   r.producto, p.nombreproducto, r.observacion, r.tipoprecio, r.esvisiblecomentarioendm, r.comentariosrelpre
+                   FROM cvp.relpre r
+                   LEFT JOIN cvp.relvis v ON r.periodo::text = v.periodo::text AND r.informante = v.informante AND r.formulario = v.formulario AND r.visita = v.visita
+                   LEFT JOIN cvp.personal pr ON v.recepcionista::text = pr.persona::text
+                   LEFT JOIN cvp.productos p ON r.producto::text = p.producto::text
+                   WHERE r.comentariosrelpre IS NOT NULL AND r.comentariosrelpre::text <> ''::text AND r.comentariosrelpre::text <> ' '::text)`
+        }        
+    },context);
 }
--- server/table-control_comentariosrelvis.js
+++ server/table-control_comentariosrelvis.js
@@ -1,26 +1,46 @@
 "use strict";
 
 module.exports = function(context){
+    var puedeEditar = context.user.usu_rol ==='ingresador' || context.user.usu_rol ==='programador' || context.user.usu_rol ==='recepcionista' || context.user.usu_rol ==='analista' || context.user.usu_rol ==='coordinador' || context.user.usu_rol ==='jefe_campo' || context.user.usu_rol ==='recep_gabinete'|| context.user.usu_rol ==='migracion'|| context.user.usu_rol ==='supervisor';
     return context.be.tableDefAdapt({
         name:'control_comentariosrelvis',
-        editable:false,
-        dbOrigin:'view',
+        tableName:'relvis',
+        editable:puedeEditar,
+        allow:{
+            insert:false,
+            delete:false,
+            update:puedeEditar,
+        },        
         fields:[
-            {name:'periodo'                      ,typeName:'text'   }, 
-            {name:'informante'                   ,typeName:'integer'},
-            {name:'visita'                       ,typeName:'integer'},
-            {name:'panel'                        ,typeName:'integer'},
-            {name:'tarea'                        ,typeName:'integer'},
-            {name:'encuestador'                  ,typeName:'text'   },
-            {name:'nombreencuestador'            ,typeName:'text'   },
-            {name:'recepcionista'                ,typeName:'text'   },
-            {name:'nombrerecepcionista'          ,typeName:'text'   },
-            {name:'rubro'                        ,typeName:'integer'},
-            {name:'nombrerubro'                  ,typeName:'text'   },
-            {name:'formulario'                   ,typeName:'integer'},
-            {name:'nombreformulario'             ,typeName:'text'   },
-            {name:'comentarios'                  ,typeName:'text'   },
+            {name:'periodo'                      ,typeName:'text'   , allow:{update:false}      }, 
+            {name:'informante'                   ,typeName:'integer', allow:{update:false}      },
+            {name:'visita'                       ,typeName:'integer', allow:{update:false}      },
+            {name:'panel'                        ,typeName:'integer', allow:{update:false}      },
+            {name:'tarea'                        ,typeName:'integer', allow:{update:false}      },
+            {name:'encuestador'                  ,typeName:'text'   , allow:{update:false}, title:'enc'},
+            {name:'nombreencuestador'            ,typeName:'text'   , allow:{update:false}      },
+            {name:'recepcionista'                ,typeName:'text'   , allow:{update:false}, title:'rec'},
+            {name:'nombrerecepcionista'          ,typeName:'text'   , allow:{update:false}      },
+            {name:'rubro'                        ,typeName:'integer', allow:{update:false}      },
+            {name:'nombrerubro'                  ,typeName:'text'   , allow:{update:false}      },
+            {name:'formulario'                   ,typeName:'integer', allow:{update:false}, title:'for'},
+            {name:'nombreformulario'             ,typeName:'text'   , allow:{update:false}      },
+            {name:'comentarios'                  ,typeName:'text'   , allow:{update:puedeEditar}, width:500},
         ],
         primaryKey:['periodo','informante','visita','formulario'],
-    });
+        sortColumns:[{column:'periodo'},{column:'panel'},{column:'tarea'},{column:'informante'},{column:'visita'},{column:'formulario'}],        
+        sql:{
+            from:`(SELECT r.periodo, r.informante, r.visita, r.panel, r.tarea, r.encuestador,
+            (pe.nombre::text || ' '::text) || pe.apellido::text AS nombreencuestador,
+            r.recepcionista, (pr.nombre::text || ' '::text) || pr.apellido::text AS nombrerecepcionista,
+            i.rubro, u.nombrerubro, r.formulario, f.nombreformulario, r.comentarios
+             FROM cvp.relvis r
+             LEFT JOIN cvp.informantes i ON r.informante = i.informante
+             LEFT JOIN cvp.personal pe ON r.encuestador::text = pe.persona::text
+             LEFT JOIN cvp.personal pr ON r.recepcionista::text = pr.persona::text
+             LEFT JOIN cvp.rubros u ON i.rubro = u.rubro
+             LEFT JOIN cvp.formularios f ON r.formulario = f.formulario
+            WHERE r.comentarios IS NOT NULL)`
+        }
+    },context);
 }
