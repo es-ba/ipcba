510e54f883e5aa23dbd5d189a0612cfe53911eae
manueldelapenna <manueldelapenna@gmail.com>
2020-04-07 12:20:52 -0300

    agrego encuestador a relevamiento

--- src/server/table-relevamiento.js
+++ src/server/table-relevamiento.js
@@ -10,19 +10,20 @@ module.exports = function(context){
             {name:'visita'         , typeName:'integer' }, 
             {name:'panel'          , typeName:'integer' },
             {name:'tarea'          , typeName:'integer' },
-            {name:'abrir'         , typeName: "text" , clientSide:'abrir'},
+            {name:'encuestador'    , typeName:'text', visible:false },
+            {name:'abrir'         , typeName: 'text', clientSide:'abrir'},
         ],
         primaryKey:['periodo', 'informante', 'visita'],
         sql:{
             isTable: false,
-            from: `(select periodo, informante, rv.visita, panel, tarea
+            from: `(select periodo, informante, rv.visita, panel, tarea, rv.encuestador
                 from relvis rv 
                     inner join periodos p using (periodo)
                     inner join personal per on rv.encuestador = per.persona 
                 where p.ingresando = 'S' and 
                       rv.fechasalida >= current_timestamp and
                       per.username = '${context.user.usu_usu}'
-                group by periodo, informante, rv.visita, panel, tarea)`
+                group by periodo, informante, rv.visita, panel, tarea, encuestador)`
         },
     },context);
 }
