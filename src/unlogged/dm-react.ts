c53f077a9260b2697b8cb079fd631dbf64dfabf2
manueldelapenna <manueldelapenna@gmail.com>
2020-04-17 13:11:13 -0300

    demo anda de nuevo

--- src/unlogged/dm-react.ts
+++ src/unlogged/dm-react.ts
@@ -365,7 +365,7 @@ async function obtenerEstructuraFromAddrParams(addrParams:AddrParamsHdr){
         var result = await my.ajax.dm2_preparar({
             //periodo: 'a2019m08', panel: 1, tarea: 1, sincronizar: false
             //periodo: 'a2019m08', panel: 3, tarea: 6, sincronizar: false
-            periodo: 'a2019m12', panel: 3, tarea: 6, informante: null, visita: null, encuestador: null, demo: true, useragent: null, token: null
+            periodo: 'a2019m12', panel: 3, tarea: 6, informante: null, visita: null, encuestador: null, demo: true, useragent: null, current_token: null
             //periodo: 'a2020m01', panel: 1, tarea: 1, encuestador: null, demo: true
         })
         estructura = result.estructura;
