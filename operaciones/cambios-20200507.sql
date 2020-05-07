89668ddc4340a9158f5d9ab6807b0be7e49d3a94
manueldelapenna <manueldelapenna@gmail.com>
2020-05-07 13:13:33 -0300

    valido que hdr no esté cargada en dm1 al abrir en dm2

--- operaciones/cambios-20200507.sql
+++ operaciones/cambios-20200507.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE FUNCTION controlar_estado_carga(
+CREATE OR REPLACE FUNCTION controlar_estado_carga(
     p_cambia_token boolean, p_periodo text, p_panel integer, p_tarea integer,
     p_informante integer DEFAULT null, p_visita integer DEFAULT null, p_formulario integer DEFAULT null, p_producto text DEFAULT null, p_observacion integer DEFAULT null
 ) returns void
@@ -47,5 +47,46 @@ BEGIN
         END IF;
     END IF;
     RETURN;
+END;
+$BODY$;
+
+CREATE OR REPLACE FUNCTION verificar_cargado_dm()
+    RETURNS trigger
+    LANGUAGE 'plpgsql' VOLATILE
+AS $BODY$
+DECLARE
+vtabla       varchar(100);
+vcambiatoken boolean;
+BEGIN
+vtabla= TG_TABLE_NAME;
+CASE
+    WHEN vtabla='relvis' THEN
+        vcambiatoken = TG_OP = 'UPDATE' AND new.token_relevamiento IS DISTINCT FROM old.token_relevamiento;
+		IF TG_OP <> 'INSERT' THEN
+            perform cvp.controlar_estado_carga(vcambiatoken, old.periodo, old.panel, old.tarea, old.informante);
+        END IF;
+        IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' AND (new.periodo, new.panel, new.tarea, new.informante)<>(old.periodo, old.panel, old.tarea, old.informante) THEN
+            perform cvp.controlar_estado_carga(vcambiatoken, new.periodo, new.panel, new.tarea, new.informante);
+        END IF;
+    WHEN vtabla='relpre' THEN
+        IF TG_OP <> 'INSERT' THEN
+            perform cvp.controlar_estado_carga(false, old.periodo, null, null, old.informante, old.visita, old.formulario);
+        END IF;
+        IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' AND (new.periodo, new.formulario)<>(old.periodo, old.formulario) THEN
+            perform cvp.controlar_estado_carga(false, new.periodo, null, null, new.informante, new.visita, new.formulario);
+        END IF;
+    WHEN vtabla='relatr' THEN
+        IF TG_OP <> 'INSERT' THEN
+            perform cvp.controlar_estado_carga(false, old.periodo, null, null, old.informante, old.visita, null, old.producto, old.observacion);
+        END IF;
+        IF TG_OP = 'INSERT' THEN
+            perform cvp.controlar_estado_carga(false, new.periodo, null, null, new.informante, new.visita, null, new.producto, new.observacion);
+        END IF;
+END CASE;
+IF TG_OP='DELETE' THEN
+   RETURN OLD;
+ELSE  
+   RETURN NEW;
+END IF;
 END;
 $BODY$;
