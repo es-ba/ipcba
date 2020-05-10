11edb93ca714c6e71f3998ad51b27f83123c46bb
ebtonini <ebtonini@gmail.com>
2020-05-10 15:23:37 -0300

    Corrijo consultas para editar fechas-salida y observaciones por informante (relinf)

--- src/server/table-relinf_fechassalida.js
+++ src/server/table-relinf_fechassalida.js
@@ -28,14 +28,18 @@ module.exports = function(context){
             {references:'informantes', fields:['informante']},
         ],
         sql:{
-            from:`(select r.periodo, min(v.panel) as panel, min(v.tarea) as tarea, r.informante, r.visita, 
-                    CASE WHEN min(v.panel) <> max(v.panel) or min(v.tarea) <> max(v.tarea) THEN 
-                       string_agg (distinct 'Panel '||v.panel||' , '||'Tarea '||v.tarea, chr(10) ORDER BY 'Panel '||v.panel||' , '||'Tarea '||v.tarea) 
-                    END as otropaneltarea, i.direccion, r.fechasalidadesde, r.fechasalidahasta 
-                   from relinf r
-                   left join informantes i on r.informante = i.informante
-                   left join relvis v on r.periodo = v.periodo and r.informante = v.informante and r.visita = v.visita
-                   group by r.periodo, r.informante, r.visita, i.direccion, r.fechasalidadesde, r.fechasalidahasta
+            from:`(select r.periodo, max(CASE WHEN v.pos = 1 THEN v.panel END) AS panel , max(CASE WHEN v.pos = 1 THEN v.tarea END) AS tarea, r.informante, r.visita, 
+                          CASE WHEN min(v.pos) <> max(v.pos) THEN 
+                            string_agg (CASE WHEN v.pos> 1 then 'Panel '||v.panel||' , '||'Tarea '||v.tarea end, chr(10) ORDER BY 'Panel '||v.panel||' , '||'Tarea '||v.tarea) 
+                          END as otropaneltarea, i.direccion, r.fechasalidadesde, r.fechasalidahasta 
+                    from relinf r
+                    left join informantes i on r.informante = i.informante
+                    left join (select periodo, informante, visita, panel, tarea, row_number() OVER (PARTITION BY periodo, informante, visita) as pos 
+                               from relvis
+                               group by periodo, informante, visita, panel, tarea
+                               order by periodo, informante, visita, panel, tarea) v 
+                               on r.periodo = v.periodo and r.informante = v.informante and r.visita = v.visita
+                    group by r.periodo, r.informante, r.visita, i.direccion, r.fechasalidadesde, r.fechasalidahasta
                 )`,
             },
     },context);
--- src/server/table-relinf_observaciones.js
+++ src/server/table-relinf_observaciones.js
@@ -30,15 +30,26 @@ module.exports = function(context){
             {references:'informantes', fields:['informante']},
         ],
         sql:{
-            from:`(select r.periodo, min(h.panel) as panel, min(h.tarea) as tarea, r.informante, string_agg (h.razon,'/' order by panel, tarea) razon, r.visita, h.direccion, 
-                CASE WHEN min(panel) <> max(panel) or min(tarea) <> max(tarea) THEN 
-                    string_agg ('Panel '||h.panel||' , '||'Tarea '||h.tarea||':'||chr(10)||h.formularioshdr,chr(10) ORDER BY panel,tarea) 
-                ELSE
-                    string_agg (h.formularioshdr,chr(10) ORDER BY panel,tarea)
-                END as formularios, h.contacto, h.ordenhdr, h.maxperiodoinformado, r.observaciones
-                from relinf r 
-                left join hdrexportarteorica h on r.periodo = h.periodo and r.informante = h.informante and r.visita = h.visita
-                group by r.periodo, r.informante, r.visita, h.direccion, h.contacto, h.ordenhdr, h.maxperiodoinformado
+            from:`(select r.periodo, max(CASE WHEN h.pos = 1 THEN h.panel END) AS panel , max(CASE WHEN h.pos = 1 THEN h.tarea END) AS tarea, 
+                       r.informante, string_agg (h.razon,'/' order by panel, tarea) as razon, r.visita, h.direccion, 
+                       CASE WHEN min(h.pos) <> max(h.pos) THEN 
+                         string_agg ('Panel '||h.panel||' , '||'Tarea '||h.tarea||':'||chr(10)||h.formularioshdr,chr(10) ORDER BY panel,tarea) 
+                       ELSE
+                         string_agg (h.formularioshdr,chr(10) ORDER BY panel,tarea)
+                       END as formularios, h.contacto, h.ordenhdr,
+                       CASE WHEN min(h.pos) <> max(h.pos) THEN 
+                         string_agg ('Panel '||h.panel||' , '||'Tarea '||h.tarea||':'||chr(10)||h.maxperiodoinformado,chr(10) ORDER BY panel,tarea) 
+                       ELSE
+                         string_agg (h.maxperiodoinformado,chr(10) ORDER BY panel,tarea)
+                       END as maxperiodoinformado, r.observaciones
+                   from relinf r 
+                   left join (SELECT periodo, informante, visita, direccion, contacto, ordenhdr, panel, tarea, maxperiodoinformado, razon, formularioshdr, 
+                                row_number() OVER (PARTITION BY periodo, informante, visita) as pos
+                              from hdrexportarteorica 
+                              group by periodo, informante, visita, direccion, contacto, ordenhdr, panel, tarea, maxperiodoinformado, razon, formularioshdr
+                              order by periodo, informante, visita, direccion, contacto, ordenhdr, panel, tarea, maxperiodoinformado, razon, formularioshdr) h 
+                              on r.periodo = h.periodo and r.informante = h.informante and r.visita = h.visita
+                    group by r.periodo, r.informante, r.visita, h.direccion, h.contacto, h.ordenhdr
                 )`,
             },
     },context);
