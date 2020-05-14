27b5e30aa555ecc84d684df9e83ae40fec9fbd22
ebtonini <ebtonini@gmail.com>
2020-05-14 12:15:24 -0300

    muestra la info de evolución del ingreso en grilla relevamiento y permiso para el supervisor

--- src/server/table-relevamiento.js
+++ src/server/table-relevamiento.js
@@ -37,30 +37,48 @@ module.exports = function(context){
         sql:{
             isTable: false,
             from: `(select rv.periodo, panel, tarea, rp.fechasalida, rv.informante, rv.visita, i.nombreinformante, i.direccion, i.contacto, i.telcontacto, i.web, i.email,
-                    case when per.labor = 'E' then per.persona else null end as encuestador,
-                    per.persona, 
+                    case when per.labor = 'E' then per.persona else null end as encuestador, per.persona, 
                     string_agg(distinct tk.username, ', ' order by tk.username) as usuario,
                     string_agg(distinct ptk.persona, ', ' order by ptk.persona) as usuario_codigo,
                     string_agg(distinct concat_ws(' ',ptk.nombre,ptk.apellido), ', ' order by concat_ws(' ',ptk.nombre,ptk.apellido)) as apenom,
-                    nullif(count(distinct pre.formulario),0) as cantformulariostotales,
-                    nullif(count(distinct case when pre.tipoprecio is not null then pre.formulario else null end),0) as cantformularioscompletos,  
-                    nullif(count(distinct pre.formulario) - count(distinct case when pre.tipoprecio is not null then pre.formulario else null end),0) as cantformulariosfaltantes,
-                    nullif(sum(case when pre.periodo is not null then 1 else 0 end),0) as cantpreciostotales,
-                    nullif(sum(case when pre.tipoprecio is not null then 1 else 0 end),0) as cantprecioscompletos,
-                    nullif(sum(case when pre.periodo is not null then 1 else 0 end) - sum(case when pre.tipoprecio is not null then 1 else 0 end),0) as cantpreciosfaltantes
-                from (select * from personal per where per.username = ${db.quoteLiteral(context.user.usu_usu)}) per
-                    inner join reltar rt on rt.encuestador = per.persona or per.labor <> 'E'
-                    inner join relvis rv using (periodo, panel, tarea)
-                    inner join periodos p using (periodo)
-                    inner join relpan rp using (periodo, panel)
-                    inner join informantes i on rv.informante = i.informante 
-                    left join tokens tk on tk.token = token_relevamiento
-                    left join personal ptk on ptk.username = tk.username
-                    left join relinf ri on rv.periodo = ri.periodo and rv.informante = ri.informante and rv.visita = ri.visita
-                    left join relpre pre on rv.periodo = pre.periodo and rv.informante = pre.informante and rv.visita = pre.visita and rv.formulario = pre.formulario
-                    where p.ingresando = 'S' 
-                    and current_date between COALESCE(ri.fechasalidadesde,rp.fechasalidadesde, rp.fechasalida) and COALESCE(ri.fechasalidahasta,rp.fechasalidahasta, rp.fechasalida) 
-                group by rv.periodo, panel, tarea, rp.fechasalida, rv.informante, rv.visita, i.nombreinformante, i.direccion, i.contacto, i.telcontacto, i.web, i.email, per.persona, per.labor)`
+                    nullif(count(distinct rv.formulario),0) as cantformulariostotales,
+                    count(distinct case when z.espositivoformulario = 'N' then rv.formulario else null end)  +
+                    count(distinct case when pre.tipoprecio is not null then pre.formulario else null end)  -
+                    (select count(*) from 
+                        regexp_matches (
+                        regexp_replace(string_agg(distinct pre.formulario::text||pre.tipo,'' order by pre.formulario::text||pre.tipo),'[NS]','', 'g')
+                        , '([0-9]{2,4})\\1'
+                        )
+                    ) --estado intermedio
+                    as cantformularioscompletos, 
+                    nullif(count(distinct rv.formulario),0) - (
+                    count(distinct case when z.espositivoformulario = 'N' then rv.formulario else null end)  +
+                    count(distinct case when pre.tipoprecio is not null then pre.formulario else null end)  -
+                    (select count(*) from 
+                        regexp_matches (
+                        regexp_replace(string_agg(distinct pre.formulario::text||pre.tipo,'' order by pre.formulario::text||pre.tipo),'[NS]','', 'g')
+                        , '([0-9]{2,4})\\1'
+                        )
+                    ) --estado intermedio
+                    ) as cantformulariosfaltantes,
+                    sum(case when z.espositivoformulario = 'S' and pre.periodo is not null then 1 else 0 end) as cantpreciostotales,
+                    sum(case when z.espositivoformulario = 'S' and pre.periodo is not null and pre.tipoprecio is not null then 1 else 0 end) as cantprecioscompletos,
+                    sum(case when z.espositivoformulario = 'S' and pre.periodo is not null then 1 else 0 end) - 
+                    sum(case when z.espositivoformulario = 'S' and pre.periodo is not null and pre.tipoprecio is not null then 1 else 0 end) as cantpreciosfaltantes
+                    from (select * from personal per where per.username = ${db.quoteLiteral(context.user.usu_usu)}) per
+                        inner join reltar rt on rt.encuestador = per.persona or per.labor <> 'E'
+                        inner join relvis rv using (periodo, panel, tarea)
+                        inner join razones z using(razon)
+                        inner join periodos p using (periodo)
+                        inner join relpan rp using (periodo, panel)
+                        inner join informantes i on rv.informante = i.informante 
+                        left join tokens tk on tk.token = token_relevamiento
+                        left join personal ptk on ptk.username = tk.username
+                        left join relinf ri on rv.periodo = ri.periodo and rv.informante = ri.informante and rv.visita = ri.visita
+                        left join (select *, case when tipoprecio is null then 'N' else 'S' end as tipo from relpre) pre on rv.periodo = pre.periodo and rv.informante = pre.informante and rv.visita = pre.visita and rv.formulario = pre.formulario
+                        where p.ingresando = 'S' 
+                        and current_date between COALESCE(ri.fechasalidadesde,rp.fechasalidadesde, rp.fechasalida) and COALESCE(ri.fechasalidahasta,rp.fechasalidahasta, rp.fechasalida)
+                    group by rv.periodo, panel, tarea, rp.fechasalida, rv.informante, rv.visita, i.nombreinformante, i.direccion, i.contacto, i.telcontacto, i.web, i.email, per.persona, per.labor)`
         },
     },context);
 }
