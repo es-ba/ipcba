3d5ad34fbb40cb13eb9af6e21d1c4770b1200de2
ebtonini <ebtonini@gmail.com>
2020-05-26 10:36:15 -0300

    mas cambios en BD para  C-pura

--- install/trg-calcular_cambioenprecio_relatr_trg.sql
+++ install/trg-calcular_cambioenprecio_relatr_trg.sql
@@ -1,16 +1,18 @@
-CREATE FUNCTION OR REPLACE cvp.calcular_cambioenprecio_relatr_trg()
+CREATE OR REPLACE FUNCTION cvp.calcular_cambioenprecio_relatr_trg()
     RETURNS trigger
     LANGUAGE 'plpgsql'
     VOLATILE NOT LEAKPROOF SECURITY DEFINER
 AS $BODY$
 DECLARE 
-	vcambio cvp.relpre.cambio%type;
+  vcambio cvp.relpre.cambio%type;
 BEGIN
  SELECT case when count(*)=0 THEN NULL ELSE 'C' END INTO vcambio
     FROM cvp.relatr_1 a
+    JOIN cvp.atributos t using (atributo)
       WHERE  a.periodo=NEW.periodo AND a.informante=NEW.informante AND a.visita=NEW.visita 
-        AND  a.producto=NEW.producto AND a.observacion=NEW.observacion and a.valor <> a.valor_1; 
- 
+        AND  a.producto=NEW.producto AND a.observacion=NEW.observacion and a.valor <> a.valor_1
+        AND not t.es_vigencia; 
+
     UPDATE cvp.relpre  
       SET cambio=vcambio
       WHERE periodo=NEW.periodo AND informante=NEW.informante 
--- operaciones/cambios-20200519.sql
+++ operaciones/cambios-20200519.sql
@@ -20,6 +20,7 @@ DECLARE
   vhayquevalidar boolean;
   --vespositivo cvp.tipopre.espositivo%type;
   vpuedecambiaratributos cvp.tipopre.puedecambiaratributos%type;
+  vcantidadatributosgenerados integer; --en el periodo siguiente
   
 BEGIN
   IF OLD.valor IS DISTINCT FROM NEW.valor THEN
@@ -31,20 +32,32 @@ BEGIN
     --  RAISE EXCEPTION 'No es posible modificar el valor del atributo cuando el campo cambio es distinto de C';
     --  RETURN NULL;
     --ELSE
-    SELECT coalesce(t.puedecambiaratributos,false) INTO vpuedecambiaratributos
+    select count(*) into vcantidadatributosgenerados
+      from cvp.relatr a
+      where a.periodo    =cvp.moverperiodos(NEW.periodo,1) AND 
+            a.producto   =NEW.producto AND
+            a.observacion=NEW.observacion AND 
+            a.informante =NEW.informante AND
+            a.visita     =NEW.visita AND 
+            a.atributo   =NEW.atributo;
+    IF vcantidadatributosgenerados>0 THEN
+      raise Exception 'Ya has sido generados los atributos del periodo sigiente; periodo %, producto % observacion % informante%, visita%, atributo % ', NEW.periodo, NEW.producto, NEW.observacion, NEW.informante, NEW.visita, NEW.atributo;
+      RETURN NULL;
+    ELSE
+      SELECT coalesce(t.puedecambiaratributos,false) INTO vpuedecambiaratributos
       FROM cvp.relpre r 
       LEFT JOIN cvp.tipopre t on r.tipoprecio = t.tipoprecio
       WHERE r.periodo=NEW.periodo AND r.informante=NEW.informante AND r.visita=NEW.visita AND r.producto=NEW.producto AND
             r.observacion=NEW.observacion;
-    IF not vpuedecambiaratributos THEN
-      RAISE EXCEPTION 'No es posible modificar el valor del atributo si el tipo de precio no lo permite';
-      RETURN NULL;
-    ELSE
-      SELECT alterable INTO valterable
+      IF not vpuedecambiaratributos THEN
+        RAISE EXCEPTION 'No es posible modificar el valor del atributo si el tipo de precio no lo permite';
+        RETURN NULL;
+      ELSE
+        SELECT alterable INTO valterable
         FROM cvp.prodatr
         WHERE producto = NEW.producto AND atributo = NEW.atributo;
-      IF valterable = 'N' THEN
-        SELECT r_1.valor_1 INTO vvalor_1
+        IF valterable = 'N' THEN
+          SELECT r_1.valor_1 INTO vvalor_1
           FROM cvp.relatr_1 r_1
           WHERE r_1.periodo=NEW.periodo AND 
                 r_1.producto=NEW.producto AND
@@ -52,27 +65,28 @@ BEGIN
                 r_1.informante=NEW.informante AND
                 r_1.visita=NEW.visita AND 
                 r_1.atributo=NEW.atributo;
-         IF vvalor_1 IS NOT NULL THEN
-           RAISE EXCEPTION 'Atributo no alterable no se puede modificar';
-           RETURN NULL;
-         ELSE
-           vvalido := false;
-           vhayquevalidar := false;
-           FOR vvaloresvalidos IN
-             SELECT valor 
+           IF vvalor_1 IS NOT NULL THEN
+             RAISE EXCEPTION 'Atributo no alterable no se puede modificar';
+             RETURN NULL;
+           ELSE
+             vvalido := false;
+             vhayquevalidar := false;
+             FOR vvaloresvalidos IN
+               SELECT valor 
                FROM cvp.valvalatr 
                WHERE producto = NEW.producto and atributo = NEW.atributo
-           LOOP
-             vhayquevalidar := true;
-             IF vvaloresvalidos.valor = NEW.valor THEN
+             LOOP
+               vhayquevalidar := true;
+               IF vvaloresvalidos.valor = NEW.valor THEN
                 vvalido := true;
-             END IF;                
-           END LOOP;
-           IF vhayquevalidar AND NOT vvalido THEN
-             RAISE EXCEPTION 'El valor ingresado no es válido para este atributo';
-             RETURN NULL;
+               END IF;                
+             END LOOP;
+             IF vhayquevalidar AND NOT vvalido THEN
+               RAISE EXCEPTION 'El valor ingresado no es válido para este atributo';
+               RETURN NULL;
+             END IF;
            END IF;
-         END IF;
+        END IF;
       END IF;
     END IF;
   END IF;
