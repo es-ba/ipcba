b0dd9ee4dd43ceedd78b1b74f091bed28b5523ea
ebtonini <ebtonini@gmail.com>
2020-05-27 16:03:41 -0300

    corrijo cambios, commit anterior

--- operaciones/cambios-20200520.sql
+++ operaciones/cambios-20200520.sql
@@ -252,49 +252,14 @@ DECLARE
   vatributos RECORD;
   vpuedecambiaratributosnew  cvp.tipopre.puedecambiaratributos%type;
   vpuedecambiaratributosold  cvp.tipopre.puedecambiaratributos%type;
-  vblanqueonew  cvp.tipopre.registrablanqueo%type;
-  vblanqueoold  cvp.tipopre.registrablanqueo%type;
 BEGIN
-  SELECT puedecambiaratributos, registrablanqueo INTO vpuedecambiaratributosnew, vblanqueonew
+  SELECT puedecambiaratributos INTO vpuedecambiaratributosnew
     FROM  cvp.tipopre
     WHERE tipoprecio=NEW.tipoprecio;
-  SELECT puedecambiaratributos, registrablanqueo INTO vpuedecambiaratributosold, vblanqueoold
+  SELECT puedecambiaratributos INTO vpuedecambiaratributosold
     FROM  cvp.tipopre
     WHERE tipoprecio=OLD.tipoprecio;
-  
-  IF OLD.tipoprecio is distinct from NEW.tipoprecio THEN
-    IF vblanqueonew and not vblanqueoold THEN
-        INSERT INTO cvp.blapre(
-            periodo, producto, observacion, informante, formulario, precio, 
-            tipoprecio, visita, modi_usu, modi_fec, modi_ope, comentariosrelpre, 
-            cambio, precionormalizado, especificacion, ultima_visita)
-        VALUES (OLD.periodo, OLD.producto, OLD.observacion, OLD.informante, OLD.formulario, OLD.precio, 
-            OLD.tipoprecio, OLD.visita, OLD.modi_usu, OLD.modi_fec, OLD.modi_ope, OLD.comentariosrelpre, 
-            OLD.cambio, OLD.precionormalizado, OLD.especificacion, OLD.ultima_visita);
-        --
-        INSERT INTO cvp.blaatr 
-            SELECT * FROM cvp.relatr 
-            WHERE periodo=NEW.periodo AND 
-               producto=NEW.producto AND
-               observacion=NEW.observacion AND 
-               informante=NEW.informante AND
-               visita=NEW.visita;  
-    END IF;
-    IF not vblanqueonew and vblanqueoold THEN
-        DELETE FROM cvp.blaatr 
-        WHERE periodo=NEW.periodo AND 
-              producto=NEW.producto AND
-              observacion=NEW.observacion AND 
-              informante=NEW.informante AND
-              visita=NEW.visita;
-        DELETE FROM cvp.blapre
-        WHERE periodo=NEW.periodo AND 
-              producto=NEW.producto AND
-              observacion=NEW.observacion AND 
-              informante=NEW.informante AND
-              visita=NEW.visita;
-    END IF;
-  END IF;
+
   IF ((NEW.cambio IS NULL AND OLD.cambio ='C') OR (not vpuedecambiaratributosnew)) AND vblanqueonew IS NOT TRUE THEN
     /*IF NEW.cambio='C' THEN --este caso solo para la segunda condicion si hubiera C
        NEW.cambio:=NULL;     --lo saco porque se solapa con la validacion de tipoprecio valido
@@ -401,4 +366,60 @@ CREATE TRIGGER relatr_calcula_cambio_precio_trg
     FOR EACH ROW
     EXECUTE PROCEDURE cvp.calcular_cambioenprecio_relatr_trg();
 
-ALTER TABLE novpre DISABLE TRIGGER novpre_cambio_trg;
+CREATE OR REPLACE FUNCTION adm_blanqueo_precios_trg()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+  vblanqueonew  cvp.tipopre.registrablanqueo%type;
+  vblanqueoold  cvp.tipopre.registrablanqueo%type;
+BEGIN
+  SELECT registrablanqueo INTO vblanqueonew
+    FROM  cvp.tipopre
+    WHERE tipoprecio=NEW.tipoprecio;
+  SELECT registrablanqueo INTO vblanqueoold
+    FROM  cvp.tipopre
+    WHERE tipoprecio=OLD.tipoprecio;
+  
+  IF OLD.tipoprecio is distinct from NEW.tipoprecio THEN
+    IF vblanqueonew and not vblanqueoold THEN
+        INSERT INTO cvp.blapre(
+            periodo, producto, observacion, informante, formulario, precio, 
+            tipoprecio, visita, modi_usu, modi_fec, modi_ope, comentariosrelpre, 
+            cambio, precionormalizado, especificacion, ultima_visita)
+        VALUES (OLD.periodo, OLD.producto, OLD.observacion, OLD.informante, OLD.formulario, OLD.precio, 
+            OLD.tipoprecio, OLD.visita, OLD.modi_usu, OLD.modi_fec, OLD.modi_ope, OLD.comentariosrelpre, 
+            OLD.cambio, OLD.precionormalizado, OLD.especificacion, OLD.ultima_visita);
+        --
+        INSERT INTO cvp.blaatr 
+            SELECT * FROM cvp.relatr 
+            WHERE periodo=NEW.periodo AND 
+               producto=NEW.producto AND
+               observacion=NEW.observacion AND 
+               informante=NEW.informante AND
+               visita=NEW.visita;  
+    END IF;
+    IF not vblanqueonew and vblanqueoold THEN
+        DELETE FROM cvp.blaatr 
+        WHERE periodo=NEW.periodo AND 
+              producto=NEW.producto AND
+              observacion=NEW.observacion AND 
+              informante=NEW.informante AND
+              visita=NEW.visita;
+        DELETE FROM cvp.blapre
+        WHERE periodo=NEW.periodo AND 
+              producto=NEW.producto AND
+              observacion=NEW.observacion AND 
+              informante=NEW.informante AND
+              visita=NEW.visita;
+    END IF;
+  END IF;
+ RETURN NEW; 
+END;
+$BODY$
+  LANGUAGE 'plpgsql' VOLATILE SECURITY DEFINER;
+
+CREATE TRIGGER relpre_adm_blanqueo_precios_trg
+    BEFORE UPDATE 
+    ON relpre
+    FOR EACH ROW
+    EXECUTE PROCEDURE adm_blanqueo_precios_trg();
