ce80bd3acf60796ae630283f3f7a03a425335c23
ebtonini <ebtonini@gmail.com>
2020-07-27 16:08:12 -0300

    filtrar precios inconsistentes que no estén en relevamiento-agregar panel,tarea,nombreinformante

--- src/server/table-precios_inconsistentes.js
+++ src/server/table-precios_inconsistentes.js
@@ -13,6 +13,9 @@ module.exports = function(context){
         fields:[
             {name:'periodo'              ,typeName:'text'   }, 
             {name:'informante'           ,typeName:'integer'},
+            {name:'nombreinformante'     ,typeName:'text'   }, 
+            {name:'panel'                ,typeName:'integer'},
+            {name:'tarea'                ,typeName:'integer'},
             {name:'formulario'           ,typeName:'integer'},
             {name:'producto'             ,typeName:'text'   }, 
             {name:'visita'               ,typeName:'integer'},
@@ -24,12 +27,19 @@ module.exports = function(context){
             {references:'formularios', fields:['formulario'] },
             {references:'productos'  , fields:['producto']   },
         ],
-        sql:{from:`(select v.periodo, v.informante, v.formulario, r.producto, v.visita, r.observacion, r.tipoprecio
+        sql:{from:`(select v.periodo, v.informante, i.nombreinformante, v.panel, v.tarea, v.formulario, r.producto, v.visita, r.observacion, r.tipoprecio
             from relvis v
+            left join informantes i on v.informante = i.informante
+            left join relinf ri on v.periodo = ri.periodo and v.informante = ri.informante and v.visita = ri.visita
+            left join reltar rt on v.periodo = rt.periodo and v.panel = rt.panel and v.panel = rt.tarea
+            left join relpan rp on v.periodo = rp.periodo and v.panel = rp.panel
             left join razones z on v.razon = z.razon
             left join relpre r on v.periodo = r.periodo and v.informante = r.informante and v.visita = r.visita and v.formulario= r.formulario
             left join tipopre t on r.tipoprecio = t.tipoprecio
-            where coalesce(z.espositivoformulario, 'N') = 'S' and coalesce(t.inconsistente, true))`
+            where coalesce(z.espositivoformulario, 'N') = 'S' and coalesce(t.inconsistente, true) and
+            not (current_timestamp between COALESCE(ri.fechasalidadesde, rt.fechasalidadesde, rp.fechasalidadesde, rp.fechasalida)+interval '9 hours'  
+            and COALESCE(ri.fechasalidahasta, rt.fechasalidahasta, rp.fechasalidahasta, rp.fechasalida) +interval '24 hours')
+            order by v.periodo, v.panel, v.tarea, v.informante, v.visita, v.formulario, r.producto, r.observacion)`
             },
     },context);
 }
