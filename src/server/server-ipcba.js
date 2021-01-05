2ba6142b1d29ef40c7e4aa46de30dc58fb2b4091
manueldelapenna <manueldelapenna@gmail.com>
2021-01-05 15:24:00 -0300

    revierto ultimo commit

--- src/server/server-ipcba.js
+++ src/server/server-ipcba.js
@@ -566,7 +566,7 @@ NETWORK:
     createResourcesForCacheJson(parameters){
         var be = this;
         var jsonResult = {};
-        const version="21-01-05";
+        const version="20-12-17";
         jsonResult.version = `#${version}`;
         jsonResult.appName = 'ipcba';
         if(parameters.periodo){
