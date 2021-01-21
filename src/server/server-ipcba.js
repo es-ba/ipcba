5ed3bafbe3aef80cc4345bc133d538b754ede832
manueldelapenna <manueldelapenna@gmail.com>
2021-01-21 11:08:59 -0300

    subo version sw

--- src/server/server-ipcba.js
+++ src/server/server-ipcba.js
@@ -569,7 +569,7 @@ NETWORK:
     createResourcesForCacheJson(parameters){
         var be = this;
         var jsonResult = {};
-        const version="21-01-20";
+        const version="21-01-21";
         jsonResult.version = `#${version}`;
         jsonResult.appName = 'ipcba';
         if(parameters.periodo){
