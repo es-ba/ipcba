b44bf6f87a72d24371a5a402212c4c1d78cf28f4
ebtonini <ebtonini@gmail.com>
2021-05-05 14:31:36 -0300

    vuelve control vigencias para visita 1 y agrego tipoprecio al control

--- install/vw-controlvigencias.sql
+++ install/vw-controlvigencias.sql
@@ -10,7 +10,8 @@ CREATE OR REPLACE VIEW controlvigencias as
             date_part('day'::text, ((((substr(moverperiodos(a.periodo::text, 1), 2, 4) || '-'::text) || substr(moverperiodos(a.periodo::text, 1), 7, 2)) || '-01'::text)::date) - '1 day'::interval) AS ultimodiadelmes,
             count(DISTINCT a.visita)::integer AS visitas,
             sum(a.valor::numeric)::integer AS vigencias,
-            string_agg((COALESCE(p.comentariosrelpre, ' '::text) || ' '::text) || COALESCE(p.observaciones, ' '::text), ' '::text) AS comentarios
+            string_agg((COALESCE(p.comentariosrelpre, ' '::text) || ' '::text) || COALESCE(p.observaciones, ' '::text), ' '::text) AS comentarios,
+			string_agg (COALESCE(p.tipoprecio, ' '::text), ' '::text order by p.visita) as tipoprecio
            FROM relvis r
              LEFT JOIN relpre p ON r.periodo::text = p.periodo::text AND r.informante = p.informante AND r.visita = p.visita AND r.formulario = p.formulario
              LEFT JOIN relatr a ON p.periodo::text = a.periodo::text AND p.producto::text = a.producto::text AND p.observacion = a.observacion AND p.informante = a.informante AND p.visita = a.visita
@@ -25,8 +26,7 @@ CREATE OR REPLACE VIEW controlvigencias as
           (COALESCE(comun.cuantos_dias_mes(a.periodo::text, d.valor::text), 0)), 
           (date_part('day'::text, ((((substr(moverperiodos(a.periodo::text, 1), 2, 4) || '-'::text) || substr(moverperiodos(a.periodo::text, 1), 7, 2)) || '-01'::text)::date) - '1 day'::interval))
           ORDER BY a.periodo, a.informante, a.producto, u.nombreproducto, a.observacion) f
-  WHERE NOT (f.visitas = 1 OR f.ultimodiadelmes = f.vigencias OR f.cantdias = f.vigencias);
-
+  WHERE NOT ((f.visitas = 1 and f.vigencias = 1) OR f.ultimodiadelmes = f.vigencias OR f.cantdias = f.vigencias);
 GRANT SELECT ON TABLE controlvigencias TO cvp_usuarios;
 GRANT SELECT ON TABLE controlvigencias TO cvp_administrador;
 
--- operaciones/cambios-20210409.sql
+++ operaciones/cambios-20210409.sql
@@ -147,7 +147,7 @@ begin
 end
 $BODY$;
 
---no se testea la consistencia de vigencias cuando hay una única visita
+--se testea la consistencia de vigencias cuando hay una única visita
 CREATE OR REPLACE VIEW controlvigencias as
  SELECT *
    FROM (SELECT a.periodo,
@@ -160,14 +160,15 @@ CREATE OR REPLACE VIEW controlvigencias as
             date_part('day'::text, ((((substr(moverperiodos(a.periodo::text, 1), 2, 4) || '-'::text) || substr(moverperiodos(a.periodo::text, 1), 7, 2)) || '-01'::text)::date) - '1 day'::interval) AS ultimodiadelmes,
             count(DISTINCT a.visita)::integer AS visitas,
             sum(a.valor::numeric)::integer AS vigencias,
-            string_agg((COALESCE(p.comentariosrelpre, ' '::text) || ' '::text) || COALESCE(p.observaciones, ' '::text), ' '::text) AS comentarios
+            string_agg((COALESCE(p.comentariosrelpre, ' '::text) || ' '::text) || COALESCE(p.observaciones, ' '::text), ' '::text) AS comentarios,
+			string_agg (COALESCE(p.tipoprecio, ' '::text), ' '::text order by p.visita) as tipoprecio
            FROM relvis r
              LEFT JOIN relpre p ON r.periodo::text = p.periodo::text AND r.informante = p.informante AND r.visita = p.visita AND r.formulario = p.formulario
              LEFT JOIN relatr a ON p.periodo::text = a.periodo::text AND p.producto::text = a.producto::text AND p.observacion = a.observacion AND p.informante = a.informante AND p.visita = a.visita
              LEFT JOIN (SELECT *
                           FROM relatr
                           WHERE atributo = 196) d 
-                          ON a.periodo = d.periodo AND a.producto = d.producto AND a.informante = d.informante AND a.observacion = d.observacion AND a.visita = d.visita
+                        ON a.periodo = d.periodo AND a.producto = d.producto AND a.informante = d.informante AND a.observacion = d.observacion AND a.visita = d.visita
              LEFT JOIN atributos t ON a.atributo = t.atributo
              LEFT JOIN productos u ON a.producto = u.producto
           WHERE t.es_vigencia AND r.razon = 1
@@ -175,4 +176,4 @@ CREATE OR REPLACE VIEW controlvigencias as
           (COALESCE(comun.cuantos_dias_mes(a.periodo::text, d.valor::text), 0)), 
           (date_part('day'::text, ((((substr(moverperiodos(a.periodo::text, 1), 2, 4) || '-'::text) || substr(moverperiodos(a.periodo::text, 1), 7, 2)) || '-01'::text)::date) - '1 day'::interval))
           ORDER BY a.periodo, a.informante, a.producto, u.nombreproducto, a.observacion) f
-  WHERE NOT (f.visitas = 1 OR f.ultimodiadelmes = f.vigencias OR f.cantdias = f.vigencias);
+  WHERE NOT ((f.visitas = 1 and f.vigencias = 1) OR f.ultimodiadelmes = f.vigencias OR f.cantdias = f.vigencias);
