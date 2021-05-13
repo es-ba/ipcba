87b359f4bbe5b9a0eb6ce41b342ee4f50b0aa2c2
manueldelapenna <manueldelapenna@gmail.com>
2021-05-13 14:10:22 -0300

    descargo hoja de estilos de bootstrap

--- src/unlogged/ejemplo-precios.tsx
+++ src/unlogged/ejemplo-precios.tsx
@@ -2726,7 +2726,7 @@ export function mostrarHdr(store:Store<HojaDeRuta, ActionHdr>, miEstructura:Estr
                     }
                 `}
             </style>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+            <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
             <OpenedTabs/>
             <AppDmIPC/>
         </Provider>,
