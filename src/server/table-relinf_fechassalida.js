05109ee264048d74624fb373d356f5b0aaf24f49
ebtonini <ebtonini@gmail.com>
2021-10-05 12:58:19 -0300

    optimizacion control atributos, modalidad en fechas salida por informante

--- src/server/table-relinf_fechassalida.js
+++ src/server/table-relinf_fechassalida.js
@@ -28,6 +28,8 @@ module.exports = function(context){
             {name:'telcontacto'            , typeName:'text'                    , allow:{update:false}      },
             {name:'web'                    , typeName:'text'                    , allow:{update:false}      },
             {name:'email'                  , typeName:'text'                    , allow:{update:false}      },
+            {name:'modalidad'              , typeName:'text'                    , allow:{update:false}      },
+            {name:'otramodalidad'          , typeName:'text'                    , allow:{update:false}      },
         ],
         primaryKey:['periodo','informante','visita'],
         foreignKeys:[
@@ -39,20 +41,28 @@ module.exports = function(context){
                           max(CASE WHEN v.pos = 1 THEN v.razon END) AS razones, 
                           CASE WHEN min(v.pos) <> max(v.pos) THEN 
                             string_agg (CASE WHEN v.pos> 1 then 'Panel '||v.panel||' , '||'Tarea '||v.tarea||coalesce(' Raz. '||v.razon,'') end, chr(10) ORDER BY 'Panel '||v.panel||' , '||'Tarea '||v.tarea||coalesce(' Raz. '||v.razon,''))  
-                          END as otropaneltarea, i.direccion, r.fechasalidadesde, r.fechasalidahasta, i.rubro, ru.nombrerubro, i.contacto, i.telcontacto, i.web, i.email 
-                    from relinf r
-                    left join informantes i on r.informante = i.informante
-                    left join rubros ru on i.rubro = ru.rubro
-                    left join (select periodo, informante, visita, panel, tarea, row_number() OVER (PARTITION BY periodo, informante, visita) as pos,
-                                      CASE WHEN min(razon) <> max(razon) THEN (min(razon) || '~'::text) || max(razon)
-                                           ELSE COALESCE(min(razon) || ''::text, NULL::text)
-                                      END AS razon
-                               from relvis
-                               group by periodo, informante, visita, panel, tarea
-                               order by periodo, informante, visita, panel, tarea) v 
-                               on r.periodo = v.periodo and r.informante = v.informante and r.visita = v.visita
-                    group by r.periodo, r.informante, r.visita, i.direccion, i.rubro, ru.nombrerubro, r.fechasalidadesde, r.fechasalidahasta, i.contacto, i.telcontacto, i.web, i.email
-                )`,
+                          END as otropaneltarea, i.direccion, r.fechasalidadesde, r.fechasalidahasta, i.rubro, ru.nombrerubro, i.contacto, i.telcontacto, i.web, i.email,
+                          max(distinct (CASE WHEN v.pos = 1 THEN v.modalidad END)) AS modalidad,
+                          CASE WHEN min(v.pos) <> max(v.pos) THEN 
+                            string_agg (CASE WHEN v.pos> 1 then 'Panel '||v.panel||' , '||'Tarea '||v.tarea||coalesce(' Mod. '||v.modalidad,'') end, chr(10) ORDER BY 'Panel '||v.panel||' , '||'Tarea '||v.tarea||coalesce(' Mod. '||v.modalidad,''))  
+                          END as otramodalidad
+                   from relinf r
+                   left join informantes i on r.informante = i.informante
+                   left join rubros ru on i.rubro = ru.rubro
+                   left join (select rv.periodo, informante, visita, rv.panel, rv.tarea, row_number() OVER (PARTITION BY rv.periodo, informante, visita) as pos,
+                               CASE WHEN min(razon) <> max(razon) THEN (min(razon) || '~'::text) || max(razon)
+                                  ELSE COALESCE(min(razon) || ''::text, NULL::text)
+                               END AS razon,
+                               CASE WHEN min(modalidad) <> max(modalidad) THEN (min(modalidad) || '~') || max(modalidad)
+                                  ELSE COALESCE(min(modalidad) || '', NULL::text)
+                               END AS modalidad
+                              from relvis rv
+                              left join reltar rt on rv.periodo = rt.periodo and rv.panel = rt.panel and rv.tarea = rt.tarea
+                              group by rv.periodo, informante, visita, rv.panel, rv.tarea
+                              order by rv.periodo, informante, visita, rv.panel, rv.tarea) v 
+                 on r.periodo = v.periodo and r.informante = v.informante and r.visita = v.visita
+                 group by r.periodo, r.informante, r.visita, i.direccion, i.rubro, ru.nombrerubro, r.fechasalidadesde, r.fechasalidahasta, i.contacto, i.telcontacto, i.web, i.email	
+                 )`,
             },
     },context);
 }
--- src/server/table-relpre_control_diccionario_atributos.js
+++ src/server/table-relpre_control_diccionario_atributos.js
@@ -20,40 +20,65 @@ module.exports = function(context){
             {name:'formulario'                   , typeName:'integer', title:'for', allow:{update:false}},
             {name:'visita'                       , typeName:'integer', title:'vis', allow:{update:false}},
             {name:'observacion'                  , typeName:'integer', title:'obs', allow:{update:false}},
-            {name:'valor'                        , typeName:'text'   , allow:{update:false}},
-            {name:'otrovalorvalidar'             , typeName:'text', title:'validos', allow:{update:false}},
-            {name:'comentariosrelpre'            , typeName:'text'    , allow:{update:puedeEditar}},
-            {name:'esvisiblecomentarioendm'      , typeName:'boolean' , title:'Ver', allow:{update:puedeEditar}}
+            {name:'comentariosrelpre'            , typeName:'text'   , allow:{update:puedeEditar}},
+            {name:'esvisiblecomentarioendm'      , typeName:'boolean', title:'Ver', allow:{update:puedeEditar}},
+            {name:'atributo'                     , typeName:'integer', allow:{update:false}},
+            {name:'valor'                        , typeName:'text'   , allow:{update:true}},
+            {name:'valido_1'                     , typeName:'text'   , allow:{update:false}},
+            {name:'otroatributo'                 , typeName:'integer', allow:{update:false}},
+            {name:'valores'                      , typeName:'text'   , allow:{update:false}},
+            {name:'valido_2'                     , typeName:'text'   , allow:{update:false}},
+            {name:'inconsistente'                , typeName:'text'   , allow:{update:false}},
         ],
         primaryKey:['periodo','producto','informante','visita','observacion'],
+        hiddenColumns:['inconsistente'],
         foreignKeys:[
             {references:'productos', fields:['producto']},
+            {references:'atributos', fields:['atributo']},
+            {references:'atributos', fields:[{source:'otroatributo', target:'atributo'}], alias:'atr'},
             {references:'informantes', fields:['informante']},
         ],
-        detailTables:[
+       detailTables:[
             {table:'relatr', abr:'ATR', label:'atributo', fields:['periodo','producto','informante','visita','observacion'], refreshParent: true},
+        ],
+        filterColumns:[
+            {column:'inconsistente', operator:'=' , value:'S'}
         ],
         sql:{
             isTable: false,
-            from:`(select periodo, panel, tarea, producto, informante, formulario, visita, observacion, comentariosrelpre, esvisiblecomentarioendm, 
-                     string_agg (concat(q.atributo,' (',at.nombreatributo,'): ',q.valor, '; '||q.otroatributo||' ('||at2.nombreatributo||'): '||q.otrovalor) ,';') as valor,
-                     string_agg (q.otrovalorvalidar,';') as otrovalorvalidar
-                   from (select a.periodo, vis.panel, vis.tarea, a.producto, a.informante, pre.formulario, a.visita, a.observacion, a.atributo, a.valor, pre.comentariosrelpre, 
-                           pre.esvisiblecomentarioendm, p.valor vvalido, p.atributo_2 otroatributo, p.valor_2 otrovalorvalidar, aa.valor otrovalor
-                           from cvp.relatr a
-                           join cvp.prodatr pa on a.producto =  pa.producto and a.atributo = pa.atributo and coalesce(pa.validaropciones, true) 
-                           join cvp.relpre pre on a.periodo = pre.periodo and a.informante = pre.informante and a.producto = pre.producto and a.visita = pre.visita and a.observacion = pre.observacion
-                           join cvp.relvis vis on pre.periodo = vis.periodo and pre.informante = vis.informante and pre.visita = vis.visita and pre.formulario = vis.formulario   
-                           left join cvp.tipopre t on pre.tipoprecio = t.tipoprecio
-                           left join cvp.prodatrval p on a.producto = p.producto and a.atributo = p.atributo and a.valor = p.valor
-                           left join cvp.relatr aa on a.periodo = aa.periodo and a.informante = aa.informante and 
-                             a.producto = aa.producto and a.visita = aa.visita and a.observacion = aa.observacion
-                             and p.atributo_2 = aa.atributo
-                           where (p.valor is null or (p.atributo_2 is not null and regexp_match(p.valor_2, aa.valor) is null)) and t.activo ='S' and t.espositivo = 'S'
-                        ) q
-                   left join cvp.atributos at on q.atributo = at.atributo
-                   left join cvp.atributos at2 on q.otroatributo = at2.atributo
-                   group by periodo, panel, tarea, producto, informante, formulario, visita, observacion, comentariosrelpre, esvisiblecomentarioendm)`,
+            from:
+            `(select periodo, panel, tarea, informante, formulario, producto, observacion, visita, comentariosrelpre, esvisiblecomentarioendm, val_valor valido_1, val_valor_2 valido_2
+                , min(case when prioridad in (1, 3) then atributo end) atributo, min(case when prioridad in (1, 3) then valor end) valor, max(case when prioridad = 2 then atributo end) otroatributo
+                , string_agg (atributo::text,', ' order by prioridad) as atributos, string_agg (valor,', ' order by prioridad) as valores, count (distinct atributo) cantatributos,
+                case when val_valor is null then 'S'
+                     when regexp_match(string_agg (valor,', ' order by prioridad), val_valor) is not null and count (distinct atributo) > 1 then 'S'
+                     when regexp_match(string_agg (valor,', ' order by prioridad), val_valor) is not null and count (distinct atributo) = 1 then 'N'
+                     when regexp_match(string_agg (valor,', ' order by prioridad), val_valor) is null then 'N'
+                     else null end as inconsistente
+                from (select a.periodo, vis.panel, vis.tarea, 
+                        a.informante, pre.formulario, a.producto, 
+                        a.observacion, a.visita, a.atributo, a.valor, pre.comentariosrelpre, pre.esvisiblecomentarioendm,
+                        case when p.producto   is null then po.producto   else p.producto   end as val_producto, 
+                        case when p.atributo   is null then po.atributo   else p.atributo   end as val_atributo, 
+                        case when p.valor      is null then po.valor      else p.valor      end as val_valor, 
+                        case when p.atributo_2 is null then po.atributo_2 else p.atributo_2 end as val_atributo_2, 
+                        case when p.valor_2    is null then po.valor_2    else p.valor_2    end as val_valor_2,
+                        case when p.producto  is not null and p.atributo  is not null and p.valor is not null  then 1
+                             when po.producto is not null and po.atributo is not null and po.valor is not null then 2
+                             else 3 end as prioridad 
+                        from cvp.relatr a
+                        join cvp.prodatr pa on a.producto =  pa.producto and a.atributo = pa.atributo  
+                        join cvp.relpre pre on a.periodo = pre.periodo and a.informante = pre.informante and a.producto = pre.producto and a.visita = pre.visita and a.observacion = pre.observacion
+                        join cvp.relvis vis on pre.periodo = vis.periodo and pre.informante = vis.informante and pre.visita = vis.visita and pre.formulario = vis.formulario   
+                        left join cvp.tipopre t on pre.tipoprecio = t.tipoprecio
+                        left join cvp.prodatrval p on a.producto = p.producto and a.atributo = p.atributo and a.valor = p.valor
+                        left join cvp.prodatrval po on a.producto = po.producto and a.atributo = po.atributo_2 and regexp_match(po.valor_2, a.valor) is null
+                        where (coalesce(pa.validaropciones, true) or po.atributo_2 is not null) and a.valor is not null /*si el a.valor es nulo, lo voy a validar o no?*/ and t.activo ='S' and t.espositivo = 'S'
+                        order by a.periodo, vis.panel, vis.tarea, 
+                             a.informante, pre.formulario, a.producto, 
+                             a.observacion, a.visita, a.atributo, pre.comentariosrelpre, pre.esvisiblecomentarioendm	
+                     ) G group by periodo, panel, tarea, informante, formulario, producto, observacion, visita, comentariosrelpre, esvisiblecomentarioendm, val_valor, val_valor_2	
+            )`,
             },
     },context);
 }
