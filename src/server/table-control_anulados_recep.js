4d2b09b662d172def21b3a25e61f33b6bbab748e
ebtonini <ebtonini@gmail.com>
2022-12-28 12:46:23 -0300

    agregado de columnas en anulados-recepcion y grilla de contrl de cambios de atributos

--- src/server/table-control_anulados_recep.js
+++ src/server/table-control_anulados_recep.js
@@ -33,6 +33,8 @@ module.exports = function(context){
             {name:'tipoprecioant'                ,typeName:'text'   ,   allow:{update:false}, title:'TPAnt'},
             {name:'comentariosrelpre'            ,typeName:'text'   ,   allow:{update:puedeEditar}},
             {name:'observaciones'                ,typeName:'text'   ,   allow:{update:esAnalista}},
+            {name:'modalidad'                    ,typeName:'text'   ,   allow:{update:false}},
+            {name:'masdatos'                     ,typeName:'text'   ,   allow:{update:false}},
         ],
         primaryKey:['periodo','producto','informante','observacion','visita'],
         foreignKeys:[
@@ -47,12 +49,25 @@ module.exports = function(context){
         ],
         sql:{
             from: `(select r.periodo, r.producto, r.informante, r.observacion, r.visita, v.panel, v.tarea, v.encuestador, v.recepcionista, 
-                  r.formulario, r.comentariosrelpre, b.precio, round(b.precionormalizado::decimal,2) as precionormalizado, b.tipoprecio, 
-                  r_1.precio_1 as precioant, round(r_1.precionormalizado_1::decimal,2) as precionormalizadoant, r_1.tipoprecio_1 as tipoprecioant, r.observaciones
-                    from relpre r left join relvis v USING (periodo,informante,visita,formulario)
-                    left join blapre b USING (periodo,producto,informante,observacion,visita)
-                    left join relpre_1 r_1 USING (periodo,producto,informante,visita,observacion)
-                    where r.tipoprecio = 'A')`,
+                r.formulario, r.comentariosrelpre, b.precio, round(b.precionormalizado::decimal,2) as precionormalizado, b.tipoprecio, 
+                r_1.precio_1 as precioant, round(r_1.precionormalizado_1::decimal,2) as precionormalizadoant, r_1.tipoprecio_1 as tipoprecioant, r.observaciones
+                ,rt.modalidad,                 
+                CASE WHEN r_1.precio_1 > 0 and r_1.precio_1 <> b.precio THEN round((b.precio/r_1.precio_1*100-100)::decimal,1)::TEXT||'%' 
+                     ELSE CASE WHEN c_1.promobs > 0 and c_1.promobs <> b.precionormalizado and r_1.precio_1 is null 
+                          THEN round((b.precionormalizado/c_1.promobs*100-100)::decimal,1)::TEXT||'%' 
+                          ELSE NULL 
+                          END 
+                     END AS masdatos
+                  from relpre r left join relvis v USING (periodo,informante,visita,formulario)
+                  left join blapre b USING (periodo,producto,informante,observacion,visita)
+                  left join relpre_1 r_1 USING (periodo,producto,informante,visita,observacion)
+                  left join reltar rt USING (periodo,panel,tarea)
+                  left join (select c.* from calobs c 
+                             join calculos_def cd on c.calculo = cd.calculo
+                             where principal) c_1
+                            on r_1.periodo_1 = c_1.periodo and r.producto = c_1.producto and r.informante = c_1.informante 
+                            and r.observacion = c_1.observacion
+                  where r.tipoprecio = 'A')`,
         }        
     }, context);
 }
--- src/server/table-control_cambios.js
+++ src/server/table-control_cambios.js
@@ -12,34 +12,74 @@ module.exports = function(context){
         },        
         fields:[
             {name:'periodo'              ,typeName:'text'   }, 
+            {name:'panel'                ,typeName:'integer'},
+            {name:'tarea'                ,typeName:'integer'},
+            {name:'modalidad'            ,typeName:'text'},
             {name:'informante'           ,typeName:'integer'},
+            {name:'formulario'           ,typeName:'integer'},
+            {name:'cambio'               ,typeName:'text'   },            
             {name:'producto'             ,typeName:'text'   }, 
             {name:'observacion'          ,typeName:'integer'},
             {name:'visita'               ,typeName:'integer'},
-            {name:'formulario'           ,typeName:'integer'},
             {name:'precio'               ,typeName:'decimal'},
             {name:'tipoprecio'           ,typeName:'text'   },
-            {name:'cambio'               ,typeName:'text'   },            
             {name:'precionormalizado'    ,typeName:'decimal'},
             {name:'precio_1'             ,typeName:'decimal'},
             {name:'tipoprecio_1'         ,typeName:'text'   },
-            {name:'atributo'             ,typeName:'integer'},
+            {name:'precionormalizado_1'  ,typeName:'decimal'},
+            {name:'masdatos'             ,typeName:'text'   },
             {name:'valor'                ,typeName:'text'   },
             {name:'valor_1'              ,typeName:'text'   },
         ],
-        primaryKey:['periodo','informante','producto','observacion','visita','atributo'],
+        primaryKey:['periodo','informante','producto','observacion','visita'],
         foreignKeys:[
             {references:'informantes', fields:['informante'] },
             {references:'formularios', fields:['formulario'] },
             {references:'productos'  , fields:['producto']   },
-            {references:'atributos'  , fields:['atributo']   },
         ],
-        sql:{from:`(select r.periodo, r.informante, r.producto, r.observacion, r.visita, r.formulario, r.precio, r.tipoprecio, 
-                      r.cambio,r.precionormalizado, r.precio_1, r.tipoprecio_1, a.atributo, a.valor, a.valor_1
-                      from relpre_1 r
-                      join relatr_1 a using(periodo, informante, producto, visita, observacion)
-                      join atributos t using (atributo)
-                      where t.es_vigencia is null and r.cambio = 'C' and a.valor is distinct from a.valor_1)`
+        sql:{from:
+            `(SELECT periodo, panel, tarea, modalidad, informante, formulario, cambio, producto, observacion, visita, 
+            precio, tipoprecio, precionormalizado, precio_1, tipoprecio_1, precionormalizado_1, masdatos,
+            string_agg(concat(atributo,'(',nombreatributo,')',':',valor), chr(10)) as valor,
+            string_agg(concat(atributo,'(',nombreatributo,')',':',valor_1), chr(10)) as valor_1
+            FROM (SELECT p.*, r_1.precio precio_1, r_1.tipoprecio tipoprecio_1, r_1.precionormalizado precionormalizado_1, a_1.valor as valor_1,
+                    CASE WHEN r_1.precio > 0 and r_1.precio <> p.precio 
+                         THEN round((p.precio/r_1.precio*100-100)::decimal,1)::TEXT||'%' 
+                         ELSE CASE WHEN c_1.promobs > 0 and c_1.promobs <> p.precionormalizado and r_1.precio is null 
+                              THEN round((p.precionormalizado/c_1.promobs*100-100)::decimal,1)::TEXT||'%' 
+                              ELSE NULL 
+                              END 
+                    END AS masdatos
+                  FROM (SELECT r.periodo, v.panel, v.tarea, rt.modalidad, r.informante, r.formulario, 
+                          r.cambio, r.producto, r.observacion, r.visita, 
+                          r.precio, r.tipoprecio, r.precionormalizado, t.atributo, t.nombreatributo, a.valor
+                        FROM relpre r
+                        JOIN relvis v ON r.periodo = v.periodo and r.informante = v.informante and r.visita = v.visita and r.formulario = v.formulario
+                        JOIN reltar rt ON r.periodo = rt.periodo and v.panel= rt.panel and v.tarea = rt.tarea
+                        JOIN relatr a on r.periodo = a.periodo and  r.informante = a.informante  and  r.producto = a.producto 
+                             and r.visita = a.visita and r.observacion = a.observacion
+                        JOIN atributos t ON  a.atributo = t.atributo
+                        WHERE r.cambio = 'C' and t.es_vigencia is null
+                       ) p 
+                  JOIN periodos per ON p.periodo = per.periodo
+                  LEFT JOIN (SELECT c.* 
+                             FROM calobs c JOIN calculos_def cd on c.calculo = cd.calculo 
+                             WHERE principal) c_1 on per.periodoanterior = c_1.periodo and p.producto = c_1.producto and 
+                             p.observacion = c_1.observacion and p.informante = c_1.informante
+                  LEFT JOIN relpre r_1 ON r_1.periodo =
+                    CASE
+                        WHEN p.visita > 1 THEN p.periodo
+                        ELSE per.periodoanterior
+                    END AND (r_1.ultima_visita = true AND p.visita = 1 OR p.visita > 1 AND r_1.visita = (p.visita - 1)) 
+                      AND r_1.informante = p.informante AND r_1.producto = p.producto AND r_1.observacion = p.observacion
+                  LEFT JOIN relatr a_1 on a_1.periodo = r_1.periodo and a_1.informante = r_1.informante and 
+                        r_1.visita = a_1.visita and r_1.observacion = a_1.observacion and r_1.producto = a_1.producto and p.atributo = a_1.atributo
+                  WHERE p.valor is distinct from a_1.valor
+                 ) Q
+            GROUP BY periodo, panel, tarea, modalidad, informante, formulario, cambio, producto, observacion, visita, 
+            precio, tipoprecio, precionormalizado, precio_1, tipoprecio_1, precionormalizado_1, masdatos
+            ORDER BY periodo, panel, tarea, modalidad, informante, formulario, cambio, producto, observacion, visita, 
+            precio, tipoprecio, precionormalizado, precio_1, tipoprecio_1, precionormalizado_1, masdatos)`
             },
     },context);
 }
