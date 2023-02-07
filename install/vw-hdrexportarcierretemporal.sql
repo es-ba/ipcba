f625d94f31b0a91fb330200518246cc8719f4dde
ebtonini <ebtonini@gmail.com>
2023-02-07 11:41:39 -0300

    contacto separado de telcontacto en hdr cierre temporal

--- install/vw-hdrexportarcierretemporal.sql
+++ install/vw-hdrexportarcierretemporal.sql
@@ -4,7 +4,7 @@ SELECT c.periodo, panel, tarea, fechasalida, c.informante, encuestador, nombreen
     WHEN min(c.razon) <> max(c.razon) THEN (min(c.razon) || '~'::text) || max(c.razon)
     ELSE COALESCE(min(c.razon) || ''::text, ''::text)
   END as razon, c.visita, c.nombreinformante, c.direccion, string_agg(formulario::text||':'||nombreformulario, '|') as formularios, 
-  (COALESCE(i.contacto, ''::character varying)::text || ' '::text) || COALESCE(i.telcontacto, ''::character varying)::text AS contacto, 
+  i.contacto::text, 
   c.conjuntomuestral, c.ordenhdr, distrito, fraccion_ant, i.comuna, i.fraccion, i.radio, i.manzana, i.depto, i.barrio, i.rubro, nombrerubro, maxperiodoinformado,
   c.observaciones, c.observaciones_campo, c.fechasalidahasta, c.modalidad, c.modalidad_ant, c.telcontacto, c.web, c.email
 FROM cvp.control_hojas_ruta c
@@ -17,7 +17,7 @@ FROM cvp.control_hojas_ruta c
                 GROUP BY informante,visita) a ON c.informante = a.informante AND c.visita = a.visita
    WHERE z.escierretemporalfor = 'S'
    GROUP BY c.periodo, panel, tarea, fechasalida, c.informante, encuestador, nombreencuestador, recepcionista, nombrerecepcionista, c.visita, c.nombreinformante, c.direccion, 
-   (COALESCE(i.contacto, ''::character varying)::text || ' '::text) || COALESCE(i.telcontacto, ''::character varying)::text,  c.conjuntomuestral, c.ordenhdr, 
+   i.contacto, c.conjuntomuestral, c.ordenhdr, 
    distrito, fraccion_ant, i.comuna, i.fraccion, i.radio, i.manzana, i.depto, i.barrio, i.rubro, nombrerubro, maxperiodoinformado,
    c.observaciones, c.observaciones_campo, c.fechasalidahasta, c.modalidad, c.modalidad_ant, c.telcontacto, c.web, c.email;
 
--- operaciones/cambios-20230206.sql
+++ operaciones/cambios-20230206.sql
@@ -53,3 +53,26 @@ CREATE OR REPLACE VIEW caldiv_vw AS
                 JOIN calculos_def d on c.calculo = d.calculo 
                 WHERE c.agrupacion = d.agrupacionprincipal and c.esproducto = 'S'
             ) cg ON c.periodo = cg.periodo AND c.calculo = cg.calculo AND  c.producto = cg.producto;
+--------------------------------------------------------------------
+CREATE OR REPLACE VIEW hdrexportarcierretemporal AS  ----informantes/hoja de ruta/cierre temporal
+SELECT c.periodo, panel, tarea, fechasalida, c.informante, encuestador, nombreencuestador, recepcionista, nombrerecepcionista, 
+  CASE
+    WHEN min(c.razon) <> max(c.razon) THEN (min(c.razon) || '~'::text) || max(c.razon)
+    ELSE COALESCE(min(c.razon) || ''::text, ''::text)
+  END as razon, c.visita, c.nombreinformante, c.direccion, string_agg(formulario::text||':'||nombreformulario, '|') as formularios, 
+  i.contacto::text, 
+  c.conjuntomuestral, c.ordenhdr, distrito, fraccion_ant, i.comuna, i.fraccion, i.radio, i.manzana, i.depto, i.barrio, i.rubro, nombrerubro, maxperiodoinformado,
+  c.observaciones, c.observaciones_campo, c.fechasalidahasta, c.modalidad, c.modalidad_ant, c.telcontacto, c.web, c.email
+FROM cvp.control_hojas_ruta c
+   LEFT JOIN cvp.razones z on c.razon = z.razon 
+   LEFT JOIN cvp.informantes i ON c.informante = i.informante
+   LEFT JOIN cvp.rubros r ON i.rubro = r.rubro
+   LEFT JOIN (SELECT informante, visita, max(periodo) AS maxperiodoinformado
+                FROM cvp.control_hojas_ruta
+                WHERE razon = 1
+                GROUP BY informante,visita) a ON c.informante = a.informante AND c.visita = a.visita
+   WHERE z.escierretemporalfor = 'S'
+   GROUP BY c.periodo, panel, tarea, fechasalida, c.informante, encuestador, nombreencuestador, recepcionista, nombrerecepcionista, c.visita, c.nombreinformante, c.direccion, 
+   i.contacto, c.conjuntomuestral, c.ordenhdr, 
+   distrito, fraccion_ant, i.comuna, i.fraccion, i.radio, i.manzana, i.depto, i.barrio, i.rubro, nombrerubro, maxperiodoinformado,
+   c.observaciones, c.observaciones_campo, c.fechasalidahasta, c.modalidad, c.modalidad_ant, c.telcontacto, c.web, c.email;
