96f0117adfb5262ef3367ad8637af725cf0d4429
ebtonini <ebtonini@gmail.com>
2023-03-07 10:19:49 -0300

    actualizaciones en la BD, corregido

--- operaciones/cambios-20230303.sql
+++ operaciones/cambios-20230303.sql
@@ -3,9 +3,10 @@ ALTER TABLE tokens ADD COLUMN tokentype text;
 ALTER TABLE tokens ADD COLUMN due timestamp without time zone;
 ALTER TABLE tokens ADD COLUMN "info" jsonb;
 
+DROP TRIGGER reltar_abi_trg ON reltar;
 CREATE TRIGGER reltar_abi_trg
-  BEFORE INSERT OR UPDATE OF supervisor, encuestador, realizada, resultado, observaciones, puntos, cargado, descargado,
-                             fechasalidadesde, fechasalidahasta, modalidad, visiblepararelevamiento OR DELETE
+  BEFORE INSERT OR DELETE OR UPDATE OF supervisor, encuestador, realizada, resultado, observaciones, puntos, cargado, descargado, id_instalacion
+  , fechasalidadesde, fechasalidahasta, modalidad, visiblepararelevamiento
   ON reltar
   FOR EACH ROW
   EXECUTE PROCEDURE verificar_ingresando();
