15f8ac35477944eabaa967ee9e64f897b5d415da
ebtonini <ebtonini@gmail.com>
2024-06-06 20:25:50 -0300

    transmitir canastas de promedios de tres calculos cerrados

--- install/vw-calgru_promedios.sql
+++ install/vw-calgru_promedios.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE VIEW calgru_promedios AS
+CREATE OR REPLACE VIEW calgru_promedios
+ AS
  SELECT c.periodo,
     c.calculo,
     c.agrupacion,
@@ -17,12 +18,14 @@ CREATE OR REPLACE VIEW calgru_promedios AS
     c.indiceredondeado,
     c.incidenciaredondeada,
     c.ponderadorimplicito,
-    (c0.valorgru + c1.valorgru + c.valorgru) / 3::double precision AS valorgrupromedio
+    CASE WHEN p1.abierto = 'N' then (c0.valorgru + c1.valorgru + c.valorgru) / 3::double precision else null end AS valorgrupromedio
    FROM cvp.calgru c
-     JOIN cvp.calculos_def cd on c.calculo = cd.calculo
-     LEFT JOIN cvp.periodos p ON c.periodo::text = p.periodo::text
-     LEFT JOIN cvp.calgru c0 ON c0.periodo::text = p.periodoanterior::text AND c.calculo = c0.calculo AND c.agrupacion::text = c0.agrupacion::text AND c.grupo::text = c0.grupo::text
-     LEFT JOIN cvp.calgru c1 ON c1.periodo::text = cvp.moverperiodos(c.periodo::text, 1) AND c1.calculo = c.calculo AND c1.agrupacion::text = c.agrupacion::text AND c1.grupo::text = c.grupo::text
+     JOIN cvp.calculos_def cd ON c.calculo = cd.calculo
+     JOIN cvp.calculos p ON c.periodo = p.periodo and c.calculo = p.calculo
+     LEFT JOIN cvp.calgru c0 ON c0.periodo = p.periodoanterior AND p.calculoanterior = c0.calculo AND c.agrupacion = c0.agrupacion AND c.grupo = c0.grupo
+     LEFT JOIN cvp.calgru c1 ON c1.periodo = cvp.moverperiodos(c.periodo, 1) AND c1.calculo = c.calculo AND c1.agrupacion = c.agrupacion AND c1.grupo = c.grupo
+     LEFT JOIN cvp.calculos p1 ON c1.periodo = p1.periodo and c1.calculo = p1.calculo
   WHERE cd.principal;
+
 
 GRANT SELECT ON TABLE calgru_promedios TO cvp_administrador;
--- operaciones/cambios-20240603.sql
+++ operaciones/cambios-20240603.sql
@@ -1,3 +1,34 @@
+set search_path = cvp;
+CREATE OR REPLACE VIEW cvp.calgru_promedios
+ AS
+ SELECT c.periodo,
+    c.calculo,
+    c.agrupacion,
+    c.grupo,
+    c.variacion,
+    c.impgru,
+    c.valorprel,
+    c.valorgru,
+    c.grupopadre,
+    c.nivel,
+    c.esproducto,
+    c.ponderador,
+    c.indice,
+    c.indiceprel,
+    c.incidencia,
+    c.indiceredondeado,
+    c.incidenciaredondeada,
+    c.ponderadorimplicito,
+    CASE WHEN p1.abierto = 'N' then (c0.valorgru + c1.valorgru + c.valorgru) / 3::double precision else null end AS valorgrupromedio
+   FROM cvp.calgru c
+     JOIN cvp.calculos_def cd ON c.calculo = cd.calculo
+     JOIN cvp.calculos p ON c.periodo = p.periodo and c.calculo = p.calculo
+     LEFT JOIN cvp.calgru c0 ON c0.periodo = p.periodoanterior AND p.calculoanterior = c0.calculo AND c.agrupacion = c0.agrupacion AND c.grupo = c0.grupo
+     LEFT JOIN cvp.calgru c1 ON c1.periodo = cvp.moverperiodos(c.periodo, 1) AND c1.calculo = c.calculo AND c1.agrupacion = c.agrupacion AND c1.grupo = c.grupo
+     LEFT JOIN cvp.calculos p1 ON c1.periodo = p1.periodo and c1.calculo = p1.calculo
+  WHERE cd.principal;
+
+-------------------------------------------------------
 CREATE OR REPLACE FUNCTION validar_transmitir_canasta_trg()
   RETURNS trigger AS
 $BODY$
@@ -47,10 +78,4 @@ RETURN NEW;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE SECURITY INVOKER;
-  
-CREATE TRIGGER calculos_controlar_transmitir_canastas_trg
-  BEFORE UPDATE
-  ON calculos
-  FOR EACH ROW
-  EXECUTE PROCEDURE validar_transmitir_canasta_trg();
-
+------------------------------------------
