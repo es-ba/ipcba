ff089ad286d61f9923ef4dce00c451bee49ac785
Sebastián <spierini@info.lifia.unlp.edu.ar>
2024-07-29 14:45:19 -0300

    se actualiza bp, se agregan nuevas columnas

---
+++ src/server/table-control_diccionario_atributos.ts
@@ -0,0 +1,56 @@
+"use strict";
+import { Context, TableDefinition } from "backend-plus";
+export const control_diccionario_atributos = (context:Context):TableDefinition =>{
+    var puedeEditar = context.user.usu_rol ==='programador' || context.user.usu_rol ==='analista' || context.user.usu_rol ==='coordinador' || context.user.usu_rol ==='jefe_campo'  || context.user.usu_rol ==='recepcionista';
+    return {
+        name:'control_diccionario_atributos',
+        tableName:'relatr',
+        editable:puedeEditar,
+        allow:{
+            insert:false,
+            delete:false,
+            update:puedeEditar,
+        },
+        fields:[
+            {name:'periodo'                      , typeName:'text'   , allow:{update:false}},
+            {name:'panel'                        , typeName:'integer', allow:{update:false}},
+            {name:'tarea'                        , typeName:'integer', allow:{update:false}},
+            {name:'producto'                     , typeName:'text'   , allow:{update:false}},
+            {name:'atributo'                     , typeName:'integer', allow:{update:false}},
+            {name:'informante'                   , typeName:'integer', title:'inf', allow:{update:false}},
+            {name:'recepcionista'                , typeName:'text'    , allow:{update:false}            },
+            {name:'formulario'                   , typeName:'integer', title:'for', allow:{update:false}},
+            {name:'visita'                       , typeName:'integer', title:'vis', allow:{update:false}},
+            {name:'observacion'                  , typeName:'integer', title:'obs', allow:{update:false}},
+            {name:'valor'                        , typeName:'text'   , allow:{update:true}, postInput:'upperSpanish'},
+            {name:'comentariosrelpre'            , typeName:'text', allow:{update:false}},
+            {name:'esvisiblecomentarioendm'      , typeName:'boolean', title:'Ver', allow:{update:false}},
+            {name:'inconsistente'                , typeName:'text', allow:{update:false}}
+        ],
+        primaryKey:['periodo','producto','informante','visita','observacion','atributo'],
+        hiddenColumns:['inconsistente'],
+        detailTables:[
+            {table:'relpre', abr:'PRE', label:'precio', fields:['periodo','producto','informante','visita','observacion'], refreshParent: true},
+        ],
+        foreignKeys:[
+            {references:'productos', fields:['producto']},
+            {references:'atributos', fields:['atributo']},
+            {references:'informantes', fields:['informante']},
+        ],
+        filterColumns:[
+            {column:'inconsistente', operator:'=' , value:'S'}
+        ],
+        sql:{
+            isTable: false,
+            from:`(select a.periodo,vis.panel, vis.recepcionista, vis.tarea, a.producto, a.atributo , a.informante, pre.formulario, a.visita, a.observacion, a.valor, pre.comentariosrelpre,
+                    pre.esvisiblecomentarioendm, case when p.valor is null then 'S' else 'N' end as inconsistente
+                    from cvp.relatr a
+                    join cvp.prodatr pa on a.producto = pa.producto and a.atributo = pa.atributo 
+                    join cvp.relpre pre on a.periodo = pre.periodo and a.informante = pre.informante and a.producto = pre.producto and a.visita = pre.visita and a.observacion = pre.observacion
+                    join cvp.relvis vis on pre.periodo = vis.periodo and pre.informante = vis.informante and pre.visita = vis.visita and pre.formulario = vis.formulario   
+                    left join cvp.prodatrval p on a.producto = p.producto and a.atributo = p.atributo and a.valor = p.valor
+                    left join cvp.tipopre t on pre.tipoprecio = t.tipoprecio
+                    where coalesce(pa.validaropciones, true) and t.activo ='S' and t.espositivo = 'S')`,
+            },
+    };
+}
---
+++ src/server/table-informantes_altamanual.ts
@@ -0,0 +1,81 @@
+"use strict";
+
+import { Context, TableDefinition } from "backend-plus";
+
+export const informantes_altamanual = (context:Context):TableDefinition=>{
+    const puedeEditar = context.user.usu_rol ==='programador'||context.user.usu_rol ==='analista'||context.user.usu_rol ==='coordinador';
+    const puedeEditarMigracion = context.user.usu_rol ==='programador'||context.user.usu_rol ==='migracion';
+    return {
+        name:'informantes_altamanual',
+        tableName:'informantes',
+        editable:puedeEditar,
+        allow:{
+            insert:puedeEditar,
+            delete:false,
+            update:puedeEditar,
+        },
+        //nombrecalle	altura	distrito	fraccion	radio	manzana
+
+        fields:[
+            {name: "generar"                  , typeName: "bigint" , editable:false, clientSide:'altamanualgenerar'},
+            {name:'informante'                , typeName:'integer' , allow:{update:puedeEditar}},
+            {name:'nombreinformante'          , typeName:'text'    , allow:{update:puedeEditar}, postInput:'upperSpanish'},
+            {name:'periodo'                   , typeName:'text'    , title:'UltimoPeriodo', editable:false},
+            {name:'ultimopanel'               , typeName:'integer' , title:'UltimoPanel', editable:false},
+            {name:'ultimatarea'               , typeName:'integer' , title:'UltimaTarea', editable:false},
+            {name:'altamanualperiodo'         , typeName:'text'    , allow:{update:puedeEditar}, title:'AltaEnPeriodo'},
+            {name:'altamanualpanel'           , typeName:'integer' , allow:{update:puedeEditar}, title:'AltaEnPanel'},
+            {name:'altamanualtarea'           , typeName:'integer' , allow:{update:puedeEditar}, title:'AltaEnTarea'},
+            {name:'masdeunpaneltarea'         , typeName:'text'    , title:'MasDeUnPanelTarea', editable:false},
+            {name:'estado'                    , typeName:'text'    , editable:false},
+            {name:'tipoinformante'            , typeName:'text'    , allow:{update:puedeEditar}, title:'TI', postInput:'upperSpanish'},
+            {name:'rubro'                     , typeName:'integer' , allow:{update:puedeEditar}},
+            {name:'provincia'                 , typeName:'text'    , allow:{update:puedeEditar||puedeEditarMigracion} , title: 'código provincia'},
+            {name:'direccion'                 , typeName:'text'    , editable:false},           
+            {name:'calle'                     , typeName:'integer' , allow:{update:puedeEditar}, postInput:'upperSpanish'},
+            {name:'altura'                    , typeName:'text'    , allow:{update:puedeEditar}},
+            {name:'comuna'                    , typeName:'integer' , allow:{update:puedeEditarMigracion} , visible:puedeEditarMigracion                              },
+            {name:'fraccion'                  , typeName:'integer' , allow:{update:puedeEditar}},
+            {name:'radio'                     , typeName:'integer' , allow:{update:puedeEditar}},
+            {name:'manzana'                   , typeName:'integer' , allow:{update:puedeEditar}},
+            {name:'contacto'                  , typeName:'text'    , allow:{update:puedeEditar}},
+            {name:'conjuntomuestral'          , typeName:'integer' , allow:{update:puedeEditar}, title:'CM'},
+            {name:'telcontacto'               , typeName:'text'    , allow:{update:puedeEditar}},
+            {name:'web'                       , typeName:'text'    , allow:{update:puedeEditar}},
+            {name:'email'                     , typeName:'text'    , allow:{update:puedeEditar}},
+            {name:'cluster'                   , typeName:'integer' , allow:{update:puedeEditar}},
+            {name:'altamanualconfirmar'       , typeName:'timestamp', allow:{update:puedeEditar}},
+            {name:'cadena'                    , typeName:'text'     , allow:{update:puedeEditar}},
+        ],
+        primaryKey:['informante'],
+        hiddenColumns:['altamanualconfirmar'],
+        detailTables:[
+            {table:'forinf', abr:'FOR', label:'formularios', fields:['informante']},
+            {table:'relvis', abr:'VIS', label:'visitas', fields:['periodo','informante']},
+        ],
+        foreignKeys:[
+            {references:'rubros'          , fields:['rubro']           },
+            {references:'tipoinf'         , fields:['tipoinformante']  },
+            {references:'calles'          , fields:['calle']            , displayFields:['nombrecalle']     },
+            {references:'provincias'      , fields:['provincia']        , displayFields:['nombreprovincia'] }
+        ],
+        sql:{
+            from:`(select i.informante, nombreinformante, ei.estado, tipoinformante, calle, comuna, provincia, direccion, rubro, altamanualperiodo, 
+                       altamanualpanel, case when cantpantar = 1 then r.panelselec else null end::integer as ultimopanel, 
+                       altamanualtarea, case when cantpantar = 1 then r.tareaselec else null end::integer as ultimatarea,
+                       case when cantpantar > 1 then varias else null end as masdeunpaneltarea, nombrecalle, altura, distrito, fraccion, radio, manzana, contacto, 
+                       telcontacto, web, email,  altamanualconfirmar, r.periodo, "cluster", conjuntomuestral, i.cadena
+                    from informantes i
+                    left join informantes_estado ei on i.informante = ei.informante 
+                    left join (select periodo, informante, cantpantar, panelselec, tareaselec, varias, dense_rank() OVER (PARTITION BY informante ORDER BY periodo desc) as orden
+                                from
+                                (select periodo, informante, count(distinct panel::text||tarea::text) cantpantar, max(panel) panelselec, max(tarea) tareaselec,
+                                        string_agg (distinct 'Panel: '||panel::text||' Tarea:'||tarea::text,chr(10) order by 'Panel: '||panel::text||' Tarea:'||tarea::text) varias
+                                from relvis
+                                group by periodo, informante
+                                order by informante, periodo desc) q) r on i.informante = r.informante 
+                    where coalesce(orden, 0) <= 1)`
+        }
+
+    };
+}
