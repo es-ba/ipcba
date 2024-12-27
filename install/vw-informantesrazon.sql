e6b39212837bee8c273c6c9186810737c2a10305
Elba Tonini <ebtonini@gmail.com>
2024-12-27 16:11:57 -0300

    corrijo vista de informantes por razón

--- install/vw-informantesrazon.sql
+++ install/vw-informantesrazon.sql
@@ -1,16 +1,35 @@
 CREATE OR REPLACE VIEW informantesrazon AS
-SELECT periodo, nullif(trim(replace(r.razon,chr(10),'')),'') as razon, 
-  (z.nombrerazon||coalesce('~'||x.nombrerazon,''))::character varying(250) as nombrerazon, 
-  sum(length(formularios)-length(replace(formularios,chr(10),''))+1)::integer as cantFormularios,
-  count(distinct informante)::integer cantInformantes
-  FROM cvp.hojaderuta r
-  LEFT JOIN cvp.razones z ON  
-   CASE WHEN strpos(r.razon,'~') > 0 THEN trim(substr(replace(r.razon,chr(10),''),1,strpos(replace(r.razon,chr(10),''),'~')-1)) ELSE trim(replace(r.razon,chr(10),'')) END = z.razon::text
-  LEFT JOIN cvp.razones x ON 
-   CASE WHEN strpos(r.razon,'~') > 0 THEN trim(substr(replace(r.razon,chr(10),''),strpos(replace(r.razon,chr(10),''),'~')+1)) ELSE '' END = x.razon::text
-  WHERE r.visita = 1
-  GROUP BY 1,2,3
-  ORDER BY 1,2,3;
+  SELECT COALESCE(q.periodo, b.periodo) periodo, COALESCE(q.razones, b.razones) razon, 
+      COALESCE(q.nombresrazones, b.nombresrazones) nombrerazon, informantes cantinformantes, formularios cantformularios
+    FROM (
+      SELECT periodo, razones, nombresrazones, COUNT(*) informantes 
+        FROM (
+          SELECT periodo, informante, STRING_AGG(razon::text, '~' ORDER BY razon) razones, 
+              STRING_AGG(nombrerazon::text, '~' ORDER BY razon) nombresrazones 
+            FROM (
+              SELECT DISTINCT r.periodo, r.informante, r.razon, z.nombrerazon
+                FROM relvis r 
+                JOIN razones z ON r.razon = z.razon
+                WHERE r.visita = 1
+            ) x
+        GROUP BY periodo, informante
+        ) w
+      GROUP BY 1,2,3
+      ) q
+    FULL OUTER JOIN (
+      SELECT periodo, razon::text AS razones, nombrerazon AS nombresrazones, COUNT(DISTINCT (informante, formulario)) formularios 
+        FROM (
+          SELECT r.periodo, r.informante, r.formulario, r.razon, z.nombrerazon
+            FROM relvis r 
+            JOIN razones z ON r.razon = z.razon
+            WHERE r.visita = 1
+        ) x
+      GROUP BY 1,2,3
+      ) b ON q.periodo = b.periodo AND q.razones = b.razones AND q.nombresrazones = b.nombresrazones
+  ORDER BY 1,split_part(COALESCE(q.razones,b.razones),'~',1)::integer, 
+    CASE WHEN split_part(COALESCE(q.razones,b.razones),'~',2) = '' 
+      THEN NULL ELSE split_part(COALESCE(q.razones,b.razones),'~',2) 
+    END ::integer NULLS FIRST, COALESCE(q.nombresrazones, b.nombresrazones);
 
 GRANT SELECT ON TABLE informantesrazon TO cvp_usuarios;
 GRANT SELECT ON TABLE informantesrazon TO cvp_recepcionista;
--- operaciones/cambios-20241128.sql
+++ operaciones/cambios-20241128.sql
@@ -160,4 +160,41 @@ LEFT JOIN (SELECT periodo, informante, visita, count(*)::integer cantformactivos
              WHERE not (s.escierredefinitivoinf = 'S' or s.escierredefinitivofor = 'S')
              GROUP BY periodo, informante, visita) ca ON X.periodo = ca.periodo AND X.informante = ca.informante AND X.visita = ca.visita
 WHERE tipo <> 'Continuo' and tipo <> ('No ingresado '||x.periodo)::text and x.visita = 1
-ORDER BY periodoanterior, informanteanterior, visitaanterior, formularioanterior;    
+ORDER BY periodoanterior, informanteanterior, visitaanterior, formularioanterior;
+
+DROP VIEW IF EXISTS informantesrazon;
+CREATE OR REPLACE VIEW informantesrazon AS
+  SELECT COALESCE(q.periodo, b.periodo) periodo, COALESCE(q.razones, b.razones) razon, 
+      COALESCE(q.nombresrazones, b.nombresrazones) nombrerazon, informantes cantinformantes, formularios cantformularios
+    FROM (
+      SELECT periodo, razones, nombresrazones, COUNT(*) informantes 
+        FROM (
+          SELECT periodo, informante, STRING_AGG(razon::text, '~' ORDER BY razon) razones, 
+              STRING_AGG(nombrerazon::text, '~' ORDER BY razon) nombresrazones 
+            FROM (
+              SELECT DISTINCT r.periodo, r.informante, r.razon, z.nombrerazon
+                FROM relvis r 
+                JOIN razones z ON r.razon = z.razon
+                WHERE r.visita = 1
+            ) x
+        GROUP BY periodo, informante
+        ) w
+      GROUP BY 1,2,3
+      ) q
+    FULL OUTER JOIN (
+      SELECT periodo, razon::text AS razones, nombrerazon AS nombresrazones, COUNT(DISTINCT (informante, formulario)) formularios 
+        FROM (
+          SELECT r.periodo, r.informante, r.formulario, r.razon, z.nombrerazon
+            FROM relvis r 
+            JOIN razones z ON r.razon = z.razon
+            WHERE r.visita = 1
+        ) x
+      GROUP BY 1,2,3
+      ) b ON q.periodo = b.periodo AND q.razones = b.razones AND q.nombresrazones = b.nombresrazones
+  ORDER BY 1,split_part(COALESCE(q.razones,b.razones),'~',1)::integer, 
+    CASE WHEN split_part(COALESCE(q.razones,b.razones),'~',2) = '' 
+      THEN NULL ELSE split_part(COALESCE(q.razones,b.razones),'~',2) 
+    END ::integer NULLS FIRST, COALESCE(q.nombresrazones, b.nombresrazones);
+
+GRANT SELECT ON TABLE informantesrazon TO cvp_usuarios;
+GRANT SELECT ON TABLE informantesrazon TO cvp_recepcionista;
