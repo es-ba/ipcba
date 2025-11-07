58d95f3a6e261139d6b5feadec22d556cd080946
manueldelapenna <manueldelapenna@gmail.com>
2025-11-07 14:49:03 -0300

    levanto BP nuevo

--- src/server/server-ipcba.js
+++ src/server/server-ipcba.js
@@ -58,6 +58,13 @@ class AppIpcba extends backendPlus.AppBackend{
         });
     }
     configStaticConfig(){
+        this.internalData={
+            filterUltimoPeriodo : '',
+            filterActualPeriodo : '',
+            filterUltimoCalculo : '',
+            filterAgrupacion : '',
+            filterExcluirCluster : 99999
+        }
         super.configStaticConfig();
         this.setStaticConfig(`
           server:
@@ -552,7 +559,7 @@ class AppIpcba extends backendPlus.AppBackend{
     addLoggedServices(opts){
         var be=this;
         super.addLoggedServices();
-        [
+        /*[
             {sufix:`manifest.manifest`     , fieldName:'archivo_manifiesto', mimeType:'text/cache-manifest'},
             {sufix:`estructura.js`         , fieldName:'archivo_estructura', mimeType:'application/javascript'},
             {sufix:`hdr.json`              , fieldName:'archivo_hdr'       , mimeType:'application/json'},
@@ -574,7 +581,7 @@ class AppIpcba extends backendPlus.AppBackend{
                     }
                 });
             })
-        });
+        });*/
         super.addLoggedServices(opts);
     }
     getProcedures(){
