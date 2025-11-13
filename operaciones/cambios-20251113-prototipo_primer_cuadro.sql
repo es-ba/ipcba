8233dbb23ff281a1b41137cc1e979d984545dcca
Elba Tonini <ebtonini@gmail.com>
2025-11-13 15:57:05 -0300

    ajustes script prototipo primer cuadro

--- operaciones/cambios-20251113-prototipo_primer_cuadro.sql
+++ operaciones/cambios-20251113-prototipo_primer_cuadro.sql
@@ -1,31 +1,48 @@
 set search_path = ccc;
 set role cvpowner;
-CREATE TABLE IF NOT EXISTS cuadros as
+CREATE TABLE IF NOT EXISTS cuadros_ccc as
 select * from cvp.cuadros where cuadro = '7';
-UPDATE cuadros
-	SET cuadro='1', 
-	descripcion='Tabla de equivalencias y unidad consumidora según tipo de canasta alimentaria para varones y mujeres y rango de edades', 
-	funcion='ccc_cuadro_up', 
-	parametro1='Tabla de equivalencias', 
-	grupo='G1', 
-	encabezado='Cuadro 1||| IPCBA. Tabla de equivalencias y unidad consumidora según tipo de canasta alimentaria para varones y mujeres y rango de edades', 
-	empalmedesde = false
-	WHERE cuadro='7';
+UPDATE cuadros_ccc
+    SET cuadro='1', 
+    descripcion='Tabla de equivalencias y unidad consumidora según tipo de canasta alimentaria para varones y mujeres y rango de edades', 
+    funcion='ccc_cuadro_up', 
+    parametro1='Tabla de equivalencias', 
+    grupo='G1', 
+    encabezado='Cuadro 1||| IPCBA. Tabla de equivalencias y unidad consumidora según tipo de canasta alimentaria para varones y mujeres y rango de edades', 
+    empalmedesde = false
+    WHERE cuadro='7';
 
-CREATE TABLE IF NOT EXISTS cuadros_funciones as
+CREATE TABLE IF NOT EXISTS cuadros_funciones_ccc as
 select * from cvp.cuadros_funciones where funcion = 'res_cuadro_up';
-update cuadros_funciones set funcion = 'ccc_cuadro_up' where funcion = 'res_cuadro_up';
+update cuadros_funciones_ccc set funcion = 'ccc_cuadro_up' where funcion = 'res_cuadro_up';
 
+do $SQL_ENANCE$
+ begin
+ PERFORM enance_table('cuadros_ccc','cuadro');
+ PERFORM enance_table('cuadros_funciones_ccc','agrupacion, funcion');
+ end
+$SQL_ENANCE$;
+
+alter table cuadros_ccc add CONSTRAINT cuadros_ccc_pkey PRIMARY KEY (cuadro);
+alter table cuadros_ccc add CONSTRAINT "texto invalido en descripcion de tabla cuadros_ccc" CHECK (comun.cadena_valida(descripcion::text, 'amplio'::text));
+alter table cuadros_ccc add CONSTRAINT "texto invalido en parametro1 de tabla cuadros_ccc" CHECK (comun.cadena_valida(parametro1::text, 'amplio'::text));
+alter table cuadros_ccc add CONSTRAINT "texto invalido en pie de tabla cuadros_ccc" CHECK (comun.cadena_valida(pie::text, 'amplio'::text));
+
+ALTER TABLE cuadros_funciones_ccc add CONSTRAINT cuadros_funciones_ccc_pkey PRIMARY KEY (funcion);
+   
+ALTER TABLE cuadros_ccc add CONSTRAINT cuadros_ccc_cuadros_funciones_ccc_fkey FOREIGN KEY (funcion) REFERENCES cuadros_funciones_ccc(funcion);
+
+drop TYPE res_col7 cascade;
 CREATE TYPE res_col7 AS (
-	renglon bigint,
-	formato_renglon text,
-	columna1 text,
-	columna2 text,
-	columna3 text,
-	columna4 text,
-	columna5 text,
-	columna6 text,
-	columna7 text
+    renglon bigint,
+    formato_renglon text,
+    columna1 text,
+    columna2 text,
+    columna3 text,
+    columna4 text,
+    columna5 text,
+    columna6 text,
+    columna7 text
 );
 
 --ccc_cuadro_up
@@ -43,7 +60,7 @@ declare
 begin
   return query select 0::bigint,'anchos'::text,
        'auto'::text,'auto'::text,'auto'::text,'auto'::text,'auto'::text,
-	   vAnchoNumeros, vAnchoNumeros;
+       vAnchoNumeros, vAnchoNumeros;
   return query select 1::bigint, 'U2.LR'::text, 'Tipo_CA'::text, 'Tipo_género'::text, 'Edad'::text, 
    'Energía (kcal)'::text, 'Unidad Consumidora'::text, 'Valorización diaria ($)'::text, 'Valorización mensual ($)'::text;
   return query select row_number() over (order by q.perfil)+100, formato_renglon
@@ -52,11 +69,11 @@ begin
                  (
                  select 
                     'D.21n' as formato_renglon, p.perfil, p.tipo, p.genero, p.edad, 
-					 replace (round(p.energia::decimal,2)::text,'.',p_separador) energia, 
-					 replace (round(p.unidcons::decimal,2)::text,'.',p_separador) unidcons, 
-					 replace (round(c.valorgru::decimal,2)::text,'.',p_separador) valor, 
-                     replace (round((c.valorgru * 30)::decimal, 2)::text,'.',p_separador) valor_mensual                    
-				 from perfiles p
+                    replace (round(p.energia::decimal,2)::text,'.',p_separador) energia, 
+                    replace (round(p.unidcons::decimal,2)::text,'.',p_separador) unidcons, 
+                    replace (round(c.valorgru::decimal,2)::text,'.',p_separador) valor, 
+                    replace (round((c.valorgru * 30)::decimal, 2)::text,'.',p_separador) valor_mensual                    
+                 from perfiles p
                  left join calgruper c on p.perfil = c.perfil
                  join cvp.calculos_def cd on c.calculo = cd.calculo
                  where grupo = parametro4
@@ -68,6 +85,9 @@ begin
                ;
 end;
 $BODY$;
+
+GRANT SELECT ON TABLE cuadros_ccc TO cvp_administrador;
+GRANT SELECT ON TABLE cuadros_funciones_ccc TO cvp_administrador;
 
 --test
 SELECT * from ccc_cuadro_up(null, 'a2025m04'::text, 'G1', false, false , 'a2022m02','.');  
