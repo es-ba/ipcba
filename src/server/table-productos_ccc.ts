e6d74aa21092e710f09b1752e2259d604104dddd
sebastian pierini <seba.i.pierini@gmail.com>
2025-12-17 17:48:36 -0300

    se actualiza producto_ccc para leer nombreproducto

--- src/server/table-productos_ccc.ts
+++ src/server/table-productos_ccc.ts
@@ -20,5 +20,11 @@ export const productos_ccc = (): TableDefinition => {
     foreignKeys: [
       { references: 'unidades', fields: [{ source: 'unidad_de_medida', target: 'unidad' }] },
     ],
+    sql: {
+      from: `(select p.producto as producto, case when esproducto_ipc then cp.nombreproducto else p.nombreproducto end as nombreproducto,
+              p.unidad_normal, p.cantidad, p.factor_correccion, p.unidad_de_medida, p.esproducto_ipc
+              from ccc.productos_ccc p
+              left join cvp.productos cp on p.producto = cp.producto)`,
+    }
   }
 }
