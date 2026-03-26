e1dae86ff861ceaf88061810618fcc758e227e68
sebastian pierini <seba.i.pierini@gmail.com>
2026-03-26 15:16:50 -0300

    se actualiza tabla grupos_ccc

--- src/server/table-grupos_ccc.ts
+++ src/server/table-grupos_ccc.ts
@@ -1,8 +1,11 @@
 'use strict';
 
-import { TableDefinition } from 'backend-plus';
+import { TableDefinition, Context } from 'backend-plus';
+import { productos_ccc } from "./table-productos_ccc";
 
-export const grupos_ccc = (): TableDefinition => {
+export const grupos_ccc = (_context:Context): TableDefinition => {
+  const productosTableDef = productos_ccc();
+  const sqlProductos = productosTableDef.sql!.from;
   return {
     editable: false,
     name: 'grupos_ccc',
@@ -25,5 +28,12 @@ export const grupos_ccc = (): TableDefinition => {
       { constraintType: 'check', consName: 'texto invalido en nombreagrupacion de tabla grupos', expr: `comun.cadena_valida(nombregrupo, 'castellano'::text)` },
       { constraintType: 'check', consName: 'Si esproducto=S => nombregrupo nulo', expr: `NOT esproducto = 'S'::text OR nombregrupo IS NULL` }
     ],
+    sql: {
+      from: `(select g.agrupacion, g.grupo, case when g.esproducto = 'S'::text then p.nombreproducto else g.nombregrupo end as nombregrupo, g.grupopadre, g.ponderador, g.nivel, g.esproducto
+              from ccc.grupos_ccc g
+              inner join ccc.agrupaciones_ccc a on a.agrupacion = g.agrupacion
+              left join (${sqlProductos}) p on g.grupo = p.producto and g.esproducto = 'S'::text)
+        `,
+    }
   };
 }
