d6ced35c1f6ae2c1ca1145c51ec5bba98b18ea29
sebastian pierini <seba.i.pierini@gmail.com>
2026-05-12 08:40:11 -0300

    se pasa archivo a ts

---
+++ src/server/table-personal_sin_carga.ts
@@ -0,0 +1,53 @@
+"use strict";
+
+import { Context, TableDefinition } from "backend-plus";
+
+export const personal_sin_carga = (_context:Context):TableDefinition => {
+return {
+        name:'personal_sin_carga',
+        fields:[
+            {name:'fecha'                            , typeName:'date'},
+            {name:'persona'                          , typeName:'text'},
+            {name:'labor'                            , typeName:'text'},
+        ],
+        primaryKey:['fecha','persona'],
+        foreignKeys:[
+            {references:'personal', fields:['persona']},
+        ],
+        sortColumns:[{column:'fecha'}, {column:'persona'}],
+        sql:{
+            from: `(SELECT p.persona, f.fecha, p.labor
+                FROM (
+                    SELECT per.persona, per.labor
+                    FROM personal per
+                    JOIN tareas t ON per.persona = t.encuestador
+                    JOIN ipcba.usuarios u ON per.username = u.usu_usu
+                    WHERE t.operativo = 'C'
+                      AND per.labor IN ('E', 'S')
+                      AND per.activo = 'S'
+                      AND u.usu_activo = true
+                ) p
+                CROSS JOIN (
+                    SELECT DISTINCT f.fecha
+                    FROM fechas f
+                    JOIN relpan rp ON f.fecha = rp.fechasalida
+                    WHERE f.seleccionada_planificacion = 'S'
+                ) f
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM reltar rt
+                    JOIN relpan rp2 ON rp2.periodo = rt.periodo AND rp2.panel = rt.panel
+                    WHERE rt.encuestador = p.persona
+                      AND f.fecha BETWEEN COALESCE(rt.fechasalidadesde, rp2.fechasalida)
+                                      AND COALESCE(rt.fechasalidahasta, rp2.fechasalida)
+                )
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM licencias l
+                    WHERE l.persona = p.persona
+                      AND f.fecha BETWEEN l.fechadesde AND l.fechahasta
+                )
+            )`,
+        }
+    }
+}
