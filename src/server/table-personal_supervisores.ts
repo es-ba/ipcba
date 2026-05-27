75ea3fe6cf2c096a367b9dcf1409f1ac80d90aab
sebastian pierini <seba.i.pierini@gmail.com>
2026-05-27 15:57:47 -0300

    se agrega en supervisor filtro por activo

--- src/server/table-personal_supervisores.ts
+++ src/server/table-personal_supervisores.ts
@@ -5,6 +5,6 @@ export const personal_supervisores = (context: Context): TableDefinition => {
   let def = personal(context);
   def.name = 'personal_supervisores';
   def.sql = def.sql || {};
-  def.sql.where = (def.sql.where ? def.sql.where + " and " : "") + "labor = 'S'";
+  def.sql.where = (def.sql.where ? def.sql.where + " and " : "") + "labor = 'S' and activo = 'S'";
   return def;
 };
