932899beb37a2cd5a658c628fc68f439ef19c338
sebastian pierini <seba.i.pierini@gmail.com>
2026-06-09 11:01:57 -0300

    se arregla conflicto

--- src/server/procedures-ipcba.js
+++ src/server/procedures-ipcba.js
@@ -1822,7 +1822,6 @@ ProceduresIpcba = [
                     left join tokens t on r.token_relevamiento = t.token
                     WHERE periodo = $1 AND panel = $2 AND tarea = $3 AND informante = $4 and visita = $5 and token_relevamiento is not null
                     group by t.username, t.useragent`
-<<<<<<< ticket-295/regenerar-panel
                 ,
                 [parameters.periodo, parameters.panel, parameters.tarea, parameters.informante, parameters.visita]
             ).fetchAll();
@@ -1848,7 +1847,7 @@ ProceduresIpcba = [
             }
             try{
                 if(!parameters.demo){
-                    var pendingRegeneration = await context.client.query(`
+                    const pendingRegeneration = await context.client.query(`
                         SELECT 1
                         FROM cambiopantar_lote l
                         JOIN cambiopantar_det d ON l.id_lote = d.id_lote
@@ -1866,37 +1865,6 @@ ProceduresIpcba = [
                     if(parameters.informante && parameters.visita){
                         await context.client.query(
                             `update relvis
-=======
-        ,
-        [parameters.periodo, parameters.panel, parameters.tarea, parameters.informante, parameters.visita]
-      ).fetchAll();
-      return { informanteAbierto: result.rowCount > 0, dispositivosAbiertos: result.rows }
-    }
-  },
-  {
-    action: 'dm2_preparar',
-    parameters: [
-      { name: 'periodo', typeName: 'text', references: 'periodos' },
-      { name: 'panel', typeName: 'integer' },
-      { name: 'tarea', typeName: 'integer' },
-      { name: 'informante', typeName: 'integer' },
-      { name: 'visita', typeName: 'integer' },
-      { name: 'demo', typeName: 'boolean' },
-    ],
-    policy: 'web',
-    coreFunction: async function (context, parameters) {
-      var be = context.be;
-      var fileResult;
-      if (SOLO_PARA_DEMO_DM) {
-        fileResult = JSON4all.parse(await fs.readFile('c:/temp/dm_cargar.txt', 'utf8'));
-      }
-      try {
-        if (!parameters.demo) {
-          //entro por pantalla de relevamiento
-          if (parameters.informante && parameters.visita) {
-            await context.client.query(
-              `update relvis
->>>>>>> master
                                 set preciosgenerados = true , token_relevamiento = null
                                 where periodo = $1 and panel = $2 and tarea = $3 and informante = $4 and visita = $5 /*and not preciosgenerados*/`
               ,
